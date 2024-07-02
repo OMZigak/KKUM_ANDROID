a12ce482a90ceb8274f2931eff58c997d03f5a3b
chanu <cw8881@naver.com>
2024-07-02 23:00:28 +0900

    #1 [setting] : 더미 파일 세팅, 데이터 바인딩

--- feature/src/main/java/com/teamkkumul/feature/MainActivity.kt
+++ feature/src/main/java/com/teamkkumul/feature/MainActivity.kt
@@ -1,33 +1,44 @@
 package com.teamkkumul.feature
 
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.teamkkumul.core.ui.base.BindingActivity
+import com.teamkkumul.core.ui.util.context.snackBar
 import com.teamkkumul.core.ui.util.context.toast
 import com.teamkkumul.core.ui.view.UiState
+import com.teamkkumul.feature.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel by viewModels<SearchViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private var _reqresAdapter: ReqresAdapter? = null
+    private val reqresAdapter: ReqresAdapter
+        get() = requireNotNull(_reqresAdapter)
+
+    override fun initView() {
+        initReqresRecyclerView()
         initObserveReqresState()
+    }
+
+    private fun initReqresRecyclerView() {
+        _reqresAdapter = ReqresAdapter().apply {
+            submitList(emptyList())
+        }
+        binding.rvReqres.adapter = reqresAdapter
     }
 
     private fun initObserveReqresState() {
         viewModel.reqresUserState.flowWithLifecycle(lifecycle).onEach { state ->
             when (state) {
-                is UiState.Success -> toast("성공")
-                is UiState.Failure -> toast("실패")
+                is UiState.Success -> reqresAdapter.submitList(state.data)
+                is UiState.Failure -> snackBar(binding.root, state.errorMessage)
                 is UiState.Loading -> toast("로딩중")
-                else -> {}
+                UiState.Empty -> Unit
             }
         }.launchIn(lifecycleScope)
     }
