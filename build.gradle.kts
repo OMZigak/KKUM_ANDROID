6c2bf88f77cd9b009dc8f9c2b2a56833414f6e06
chanu <cw8881@naver.com>
2024-07-02 21:43:39 +0900

    #1 [setting] : project단위 hilt의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
