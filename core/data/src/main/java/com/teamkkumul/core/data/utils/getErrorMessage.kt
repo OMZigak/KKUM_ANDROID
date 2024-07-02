a17357810e18c4c8b64d8ef01a7feed2c3b9d188
chanu <cw8881@naver.com>
2024-07-02 21:39:02 +0900

    #1 [setting] : error handling관련 로직 추가

---
+++ core/data/src/main/java/com/teamkkumul/core/data/utils/getErrorMessage.kt
@@ -0,0 +1,37 @@
+package com.teamkkumul.core.data.utils
+
+import com.teamkkumul.core.network.dto.response.BaseResponse
+import com.teamkkumul.model.network.ApiError
+import com.teamkkumul.model.network.NetWorkConnectError
+import kotlinx.serialization.json.Json
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+fun Throwable.getErrorMessage(): String {
+    return when (this) {
+        is HttpException -> {
+            val errorBody = response()?.errorBody()?.string() ?: return "Unknown error"
+            val errorResponse = Json.decodeFromString<BaseResponse<Unit>>(errorBody)
+            errorResponse.message
+        }
+
+        else -> "An unknown error occurred."
+    }
+}
+
+fun <T> Throwable.handleThrowable(): Result<T> {
+    return Result.failure(
+        when (this) {
+            is HttpException -> ApiError(this.getErrorMessage())
+            is IOException -> NetWorkConnectError("인터넷에 연결해 주세요")
+            else -> this
+        },
+    )
+}
+
+fun Response<*>?.getResponseErrorMessage(): String {
+    val errorBody = this?.errorBody()?.string() ?: return "Unknown error"
+    val errorResponse = Json.decodeFromString<BaseResponse<Unit>>(errorBody)
+    return errorResponse.message
+}
