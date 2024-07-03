1624de4634e2307d0c35230356d7c3e38904f442
yskim6772 <yskim6772@gmail.com>
2024-07-03 17:32:55 +0900

    #4 [feat] : fragment 3개 구현

---
+++ feature/src/main/java/com/teamkkumul/feature/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.teamkkumul.feature.home
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentHomeBinding
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initView() {
+    }
+}
---
+++ feature/src/main/java/com/teamkkumul/feature/mypage/MyPageFragment.kt
@@ -0,0 +1,10 @@
+package com.teamkkumul.feature.mypage
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentMyPageBinding
+
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun initView() {
+    }
+}
