b3ab67c5df07eda2d67eb8734bb477f1692c9636
yskim6772 <yskim6772@gmail.com>
2024-07-05 18:52:49 +0900

    #16 [feat] : Theme에 폰트 시스템 적용

--- core/designsystem/src/main/java/com/teamkkumul/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamkkumul/core/designsystem/theme/Theme.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -33,6 +36,25 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+
+private val LocalKkumulTypography = staticCompositionLocalOf<KkumulTypography> {
+    error("No KkumulTypography provided")
+}
+
+object KkumulTheme {
+    val typography: KkumulTypography
+        @Composable get() = LocalKkumulTypography.current
+}
+
+@Composable
+fun ProvideKkumulTypography(typography: KkumulTypography, content: @Composable () -> Unit) {
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+    CompositionLocalProvider(
+        LocalKkumulTypography provides provideTypography,
+        content = content,
+    )
+}
 
 @Composable
 fun KkumulAndroidTheme(
@@ -59,9 +81,12 @@ fun KkumulAndroidTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    val typography = kkumulTypography()
+
+    ProvideKkumulTypography(typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
 }
