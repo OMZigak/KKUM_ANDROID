0ff01c7803654ec8b24dba9f0403c33b42750c82
yskim6772 <yskim6772@gmail.com>
2024-07-05 17:26:53 +0900

    #16 [chore] : 컴포즈 primary 컬러 설정

--- core/designsystem/src/main/java/com/teamkkumul/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teamkkumul/core/designsystem/theme/Theme.kt
@@ -17,15 +17,11 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = MainColor,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = MainColor,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
