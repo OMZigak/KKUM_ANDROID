2ab0c198f0c98f2ba1b5ea4351ad41358c1b827a
yskim6772 <yskim6772@gmail.com>
2024-07-09 03:07:21 +0900

    #24 [feat] : 프로필 설정 후 반가워요 화면 구현

---
+++ feature/src/main/java/com/teamkkumul/feature/profile/WelcomeFragment.kt
@@ -0,0 +1,10 @@
+package com.teamkkumul.feature.profile
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : BindingFragment<FragmentWelcomeBinding>(R.layout.fragment_welcome) {
+    override fun initView() {
+    }
+}
