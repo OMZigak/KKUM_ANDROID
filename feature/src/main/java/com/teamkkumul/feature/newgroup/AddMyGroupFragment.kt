0c3b9544e088cd912c93b31ea34fed39c64acd7e
yskim6772 <yskim6772@gmail.com>
2024-07-09 07:45:31 +0900

    #35 [feat] : 내 모음 추가하기 첫 뷰 UI 구현

---
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/AddMyGroupFragment.kt
@@ -0,0 +1,10 @@
+package com.teamkkumul.feature.newgroup
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentAddMyGroupBinding
+
+class AddMyGroupFragment : BindingFragment<FragmentAddMyGroupBinding>(R.layout.fragment_add_my_group) {
+    override fun initView() {
+    }
+}
