321e81312c6ea145e910f2224a65cd4fd343aa93
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-10 02:23:06 +0900

    [chore/#25] Dto @Serializable, @SerialName 추가

--- core/network/src/main/java/com/teamkkumul/core/network/dto/response/ResponseMyGroupFriendDto.kt
+++ core/network/src/main/java/com/teamkkumul/core/network/dto/response/ResponseMyGroupFriendDto.kt
@@ -1,12 +1,22 @@
 package com.teamkkumul.core.network.dto.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ResponseMyGroupFriendDto(
+    @SerialName("member_count")
     val memberCount: Int,
+    @SerialName("members")
     val members: List<Member>,
 ) {
+    @Serializable
     data class Member(
+        @SerialName("id")
         val id: Int,
+        @SerialName("name")
         val name: String,
+        @SerialName("profile_img")
         val profileImg: String,
     )
 }
