2d208a87fded81d3805f5d9842314e36c68218af
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-10 04:03:26 +0900

    [feat/#36] meet up detail model 생성

---
+++ core/model/src/main/java/com/teamkkumul/model/MeetUpDetailFriendModel.kt
@@ -0,0 +1,12 @@
+package com.teamkkumul.model
+
+class MeetUpDetailFriendModel(
+    val memberCount: Int,
+    val participants: List<Participant>,
+) {
+    data class Participant(
+        val id: Int,
+        val name: String,
+        val profileImg: String,
+    )
+}
