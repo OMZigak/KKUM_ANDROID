3ca7dd7df0ba53af83402936a850c3c28a3e63c5
yskim6772 <yskim6772@gmail.com>
2024-07-09 17:03:08 +0900

    #35 [add] : enter invitation code fragment 생성

---
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/EnterInvitationCodeFragment.kt
@@ -0,0 +1,10 @@
+package com.teamkkumul.feature.newgroup
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentEnterInvitationCodeBinding
+
+class EnterInvitationCodeFragment : BindingFragment<FragmentEnterInvitationCodeBinding>(R.layout.fragment_enter_invitation_code) {
+    override fun initView() {
+    }
+}
