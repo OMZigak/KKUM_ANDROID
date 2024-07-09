02c0d9fc2c5cf3ea67207b41f959b639c89fbd39
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-10 04:06:08 +0900

    [feat/#36] MeetUpDetailFriendViewModel mock data 생성

---
+++ feature/src/main/java/com/teamkkumul/feature/meetup/MeetUpDetailFriendViewModel.kt
@@ -0,0 +1,22 @@
+package com.teamkkumul.feature.meetup
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.teamkkumul.model.MeetUpSealedItem
+
+class MeetUpDetailFriendViewModel : ViewModel() {
+    private val _members = MutableLiveData<List<MeetUpSealedItem.Participant>>()
+    val members: LiveData<List<MeetUpSealedItem.Participant>> get() = _members
+
+    init {
+        val mockMembers = listOf(
+            MeetUpSealedItem.Participant(id = 2, name ="Eric", profileImg = "https://example.com/alice.jpg" ),
+            MeetUpSealedItem.Participant(id = 3, name ="Bob", profileImg = "https://example.com/alice.jpg" ),
+            MeetUpSealedItem.Participant(id = 2, name ="Alice", profileImg = "https://example.com/alice.jpg" ),
+            MeetUpSealedItem.Participant(id = 2, name ="Eric", profileImg = "https://example.com/alice.jpg" ),
+            MeetUpSealedItem.Participant(id = 3, name ="Bob", profileImg = "https://example.com/alice.jpg" ),
+            MeetUpSealedItem.Participant(id = 2, name ="Alice", profileImg = "https://example.com/alice.jpg" ),
+        )
+    }
+}
