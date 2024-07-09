a56ca5940ba34d753d48ae2715177d7d2e26f7c0
yskim6772 <yskim6772@gmail.com>
2024-07-10 06:51:04 +0900

    #35 [chore] : viewmodel 네이밍 수정

--- feature/src/main/java/com/teamkkumul/feature/signup/NameViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/signup/NameViewModel.kt
@@ -7,7 +7,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SetNameViewModel @Inject constructor() : ViewModel() {
+class NameViewModel @Inject constructor() : ViewModel() {
     private val _inputName = MutableLiveData<String>()
     val inputName: LiveData<String> get() = _inputName
 
