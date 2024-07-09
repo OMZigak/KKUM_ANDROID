0d86504c0453c66a82223daaf5e02df43660258c
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-10 02:24:14 +0900

    [add/#25] Dto에 @Serializable, @SerialName 추가

--- core/network/src/main/java/com/teamkkumul/core/network/dto/response/ResponseMyGroupMeetUpDto.kt
+++ core/network/src/main/java/com/teamkkumul/core/network/dto/response/ResponseMyGroupMeetUpDto.kt
@@ -1,14 +1,26 @@
 package com.teamkkumul.core.network.dto.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ResponseMyGroupMeetUpDto(
+    @SerialName("promise")
     val promises: List<Promise>,
 ) {
+    @Serializable
     data class Promise(
+        @SerialName("dDay")
         val dDay: Int,
+        @SerialName("date")
         val date: String,
+        @SerialName("id")
         val id: Int,
+        @SerialName("name")
         val name: String,
+        @SerialName("place_name")
         val placeName: String,
+        @SerialName("time")
         val time: String,
     )
 }
