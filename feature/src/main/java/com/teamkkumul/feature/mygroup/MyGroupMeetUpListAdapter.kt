852f23118a5a7f6a9b450bb697a9026afef866c3
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-10 21:47:03 +0900

    [chore/#39] MyGroupMeetUpListAdapter ViewHolder변경

--- feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupMeetUpListAdapter.kt
+++ feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupMeetUpListAdapter.kt
@@ -3,7 +3,6 @@ package com.teamkkumul.feature.mygroup
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.teamkkumul.core.ui.view.ItemDiffCallback
 import com.teamkkumul.feature.databinding.ItemMyGroupRemainMeetUpBinding
 import com.teamkkumul.feature.mygroup.viewholder.MyGroupDetailMeetUpViewHolder
@@ -11,8 +10,11 @@ import com.teamkkumul.model.MyGroupMeetUpModel
 
 class MyGroupMeetUpListAdapter(
     private val onMeetUpDetailBtnClicked: () -> Unit,
-) : ListAdapter<MyGroupMeetUpModel.Promise, RecyclerView.ViewHolder>(DiffUtil) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyGroupDetailMeetUpViewHolder {
+) : ListAdapter<MyGroupMeetUpModel.Promise, MyGroupDetailMeetUpViewHolder>(DiffUtil) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): MyGroupDetailMeetUpViewHolder {
         val binding = ItemMyGroupRemainMeetUpBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -21,10 +23,8 @@ class MyGroupMeetUpListAdapter(
         return MyGroupDetailMeetUpViewHolder(binding, onMeetUpDetailBtnClicked)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is MyGroupDetailMeetUpViewHolder -> holder.onBind(getItem(position))
-        }
+    override fun onBindViewHolder(holder: MyGroupDetailMeetUpViewHolder, position: Int) {
+        holder.onBind(getItem(position))
     }
 
     companion object {
