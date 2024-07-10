8f29c449f92e31f3e5e7fa39919e0a2313043f30
chanu <cw8881@naver.com>
2024-07-10 21:06:41 +0900

    #27 [feat] : 홈프레그먼트 버튼 state flow collect

--- feature/src/main/java/com/teamkkumul/feature/home/HomeFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/home/HomeFragment.kt
@@ -1,17 +1,121 @@
 package com.teamkkumul.feature.home
 
-import androidx.navigation.fragment.findNavController
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import com.google.android.material.button.MaterialButton
 import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.core.ui.util.fragment.colorOf
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentHomeBinding
+import com.teamkkumul.feature.home.model.BtnState
+import com.teamkkumul.feature.utils.AnimateProgressBarCommon
+import com.teamkkumul.feature.utils.getCurrentTime
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val viewModel by viewModels<HomeViewModel>()
+
     override fun initView() {
-        binding.btnHome.setOnClickListener {
-            findNavController().navigate(R.id.action_fragment_home_to_homeDetailFragment)
+        initHomeBtnClick()
+        initObserveBtnState()
+    }
+
+    private fun initHomeBtnClick() {
+        initReadyBtnClick()
+        initMovingBtnClick()
+        initArriveBtnClick()
+    }
+
+    private fun animateProgressBar(progressBar: ProgressBar, progress: Int) {
+        val anim = AnimateProgressBarCommon(progressBar, 0f, progress.toFloat())
+        anim.duration = 500
+        progressBar.startAnimation(anim)
+    }
+
+    private fun initReadyBtnClick() = with(binding) {
+        btnHomeReady.setOnClickListener {
+            viewModel.clickReadyBtn()
+            tvHomeReadyTime.text = getCurrentTime()
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeReady, progressNum)
+            }
         }
-        binding.btnCompose.setOnClickListener {
-            findNavController().navigate(R.id.action_fragment_home_to_exampleComposeFragment)
+    }
+
+    private fun initMovingBtnClick() = with(binding) {
+        btnHomeMoving.setOnClickListener {
+            viewModel.clickMovingStartBtn()
+            tvHomeMovingTime.text = getCurrentTime()
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeMoving, progressNum)
+            }
         }
+    }
+
+    private fun initArriveBtnClick() = with(binding) {
+        btnHomeArrive.setOnClickListener {
+            viewModel.clickCompletedBtn()
+            tvvHomeArriveTime.text = getCurrentTime()
+
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeArrive, progressNum)
+                delay(300L)
+                animateProgressBar(pgHomeArriveEnd, progressNum)
+            }
+        }
+    }
+
+    private fun initObserveBtnState() = with(binding) {
+        observeBtnState(
+            viewModel.readyBtnState,
+            btnHomeReady,
+            ivHomeReadyCircle,
+        )
+        observeBtnState(
+            viewModel.movingStartBtnState,
+            binding.btnHomeMoving,
+            ivHomeMovingCircle,
+        )
+        observeBtnState(
+            viewModel.completedBtnState,
+            binding.btnHomeArrive,
+            ivHomeArriveCircle,
+        )
+    }
+
+    private fun observeBtnState(
+        stateFlow: StateFlow<BtnState>,
+        button: MaterialButton,
+        circle: ImageView,
+    ) {
+        stateFlow.flowWithLifecycle(viewLifeCycle).onEach { state ->
+            setUpButton(state, button, circle)
+        }.launchIn(viewLifeCycleScope)
+    }
+
+    private fun setUpButton(
+        state: BtnState,
+        button: MaterialButton,
+        circle: ImageView,
+    ) {
+        button.apply {
+            setStrokeColorResource(state.strokeColor)
+            setTextColor(colorOf(state.textColor))
+            setBackgroundColor(colorOf(state.backGroundColor))
+            isEnabled = state.isEnabled
+        }
+        circle.setImageResource(state.circleImage)
+    }
+
+    companion object {
+        private const val progressNum = 100
     }
 }
