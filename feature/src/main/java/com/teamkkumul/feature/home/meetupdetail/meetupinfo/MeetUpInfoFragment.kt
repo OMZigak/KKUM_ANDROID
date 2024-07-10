9a5c02eaefa6d0d473a2f834b5c1996e526ad6b1
chanu <cw8881@naver.com>
2024-07-11 03:41:13 +0900

    #41 [add] : 약속 상세정보 탭 프레그먼트 add

---
+++ feature/src/main/java/com/teamkkumul/feature/home/meetupdetail/meetupinfo/MeetUpInfoFragment.kt
@@ -0,0 +1,11 @@
+package com.teamkkumul.feature.home.meetupdetail.meetupinfo
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentMeetUpInfoBinding
+
+class MeetUpInfoFragment :
+    BindingFragment<FragmentMeetUpInfoBinding>(R.layout.fragment_meet_up_info) {
+    override fun initView() {
+    }
+}
---
+++ feature/src/main/java/com/teamkkumul/feature/home/meetupdetail/readystatus/ReadyStatusFragment.kt
@@ -0,0 +1,11 @@
+package com.teamkkumul.feature.home.meetupdetail.readystatus
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentReadyStatusBinding
+
+class ReadyStatusFragment :
+    BindingFragment<FragmentReadyStatusBinding>(R.layout.fragment_ready_status) {
+    override fun initView() {
+    }
+}
