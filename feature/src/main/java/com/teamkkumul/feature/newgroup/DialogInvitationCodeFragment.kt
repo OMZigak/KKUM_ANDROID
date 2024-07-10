41d18425b7bed6d03727798377421e3aacc07f25
yskim6772 <yskim6772@gmail.com>
2024-07-10 23:33:41 +0900

    #35 [feat] : 클립보드 복사하기 구현

--- feature/src/main/java/com/teamkkumul/feature/newgroup/DialogInvitationCodeFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/DialogInvitationCodeFragment.kt
@@ -1,7 +1,11 @@
 package com.teamkkumul.feature.newgroup
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.teamkkumul.core.ui.base.BindingDialogFragment
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentDialogInvitationCodeBinding
@@ -25,6 +29,15 @@ class DialogInvitationCodeFragment :
     }
 
     override fun initView() {
-        // Initialize your views here
+        binding.ivBtnCopy.setOnClickListener {
+            copyToClipboard(binding.tvInvitationCode.text.toString())
+        }
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("invitation_code", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(requireContext(), "클립보드에 복사되었습니다", Toast.LENGTH_SHORT).show()
     }
 }
