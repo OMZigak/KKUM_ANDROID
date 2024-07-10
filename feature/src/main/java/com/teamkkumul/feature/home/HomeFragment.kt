c1c7f7fdd8607e02a9fa6adfa7d4b14e3a60a0ff
yskim6772 <yskim6772@gmail.com>
2024-07-11 04:25:36 +0900

    #35 [chore] : 구현 확인 위해 navigation 추가

--- feature/src/main/java/com/teamkkumul/feature/home/HomeFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.teamkkumul.feature.home
 
+import androidx.navigation.fragment.findNavController
 import com.teamkkumul.core.ui.base.BindingFragment
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentHomeBinding
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initView() {
+        binding.btnCompose.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_home_to_fragment_add_my_group)
+        }
     }
 }
