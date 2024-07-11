388779f0015b877727b94ef4667dc507322fbdea
yskim6772 <yskim6772@gmail.com>
2024-07-11 21:14:37 +0900

    #35 [chore] : 내 모임 추가하기 백스택 제거 관리

--- feature/src/main/java/com/teamkkumul/feature/newgroup/addmygroup/AddMyGroupCompleteFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/addmygroup/AddMyGroupCompleteFragment.kt
@@ -1,5 +1,6 @@
 package com.teamkkumul.feature.newgroup.addmygroup
 
+import androidx.navigation.fragment.findNavController
 import com.teamkkumul.core.ui.base.BindingFragment
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentAddMyGroupCompleteBinding
@@ -7,5 +8,8 @@ import com.teamkkumul.feature.databinding.FragmentAddMyGroupCompleteBinding
 class AddMyGroupCompleteFragment :
     BindingFragment<FragmentAddMyGroupCompleteBinding>(R.layout.fragment_add_my_group_complete) {
     override fun initView() {
+        binding.btnOkay.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_add_my_group_complete_to_fragment_add_my_group)
+        }
     }
 }
