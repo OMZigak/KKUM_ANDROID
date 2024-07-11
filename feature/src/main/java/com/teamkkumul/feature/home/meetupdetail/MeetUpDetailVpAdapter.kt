155179f0833e0af0111c9d2a50e88c78de189c75
chanu <cw8881@naver.com>
2024-07-12 04:07:27 +0900

    #41 [refact] : viewpager 프레그먼트 변경

--- feature/src/main/java/com/teamkkumul/feature/home/meetupdetail/MeetUpDetailVpAdapter.kt
+++ feature/src/main/java/com/teamkkumul/feature/home/meetupdetail/MeetUpDetailVpAdapter.kt
@@ -3,15 +3,15 @@ package com.teamkkumul.feature.home.meetupdetail
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.teamkkumul.feature.home.meetupdetail.lateperson.LatePersonFragment
-import com.teamkkumul.feature.home.meetupdetail.meetupinfo.MeetUpInfoFragment
 import com.teamkkumul.feature.home.meetupdetail.readystatus.ReadyStatusFragment
+import com.teamkkumul.feature.home.meetupdetail.meetupinfo.MeetUpDetailFragment
 
 class MeetUpDetailVpAdapter(fr: Fragment) : FragmentStateAdapter(fr) {
     override fun getItemCount(): Int = TOTAL_TAG_NUM
 
     override fun createFragment(position: Int): Fragment { // 포지션에 따라 어떤 프레그먼트를 보여줄것인지
         return when (position) {
-            0 -> MeetUpInfoFragment()
+            0 -> MeetUpDetailFragment()
             1 -> ReadyStatusFragment()
             2 -> LatePersonFragment()
             else -> throw IllegalArgumentException("Invalid position")
