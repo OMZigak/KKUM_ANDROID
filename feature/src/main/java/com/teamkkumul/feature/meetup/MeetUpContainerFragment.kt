8c60260c16b8339ca81ced7c3bc0e5d04b0bb2be
chanu <cw8881@naver.com>
2024-07-12 04:16:28 +0900

    Revert "#41 [chore] : 불 필요한 코드 제거"
    This reverts commit 96e01c8de4a8415a703f95459b2a49cc94c77aa2.

--- feature/src/main/java/com/teamkkumul/feature/meetup/MeetUpContainerFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/MeetUpContainerFragment.kt
@@ -1,5 +1,6 @@
 package com.teamkkumul.feature.meetup
 
+import com.google.android.material.tabs.TabLayoutMediator
 import com.teamkkumul.core.ui.base.BindingFragment
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentMeetUpContainerBinding
@@ -13,6 +14,23 @@ class MeetUpContainerFragment :
 
     private fun initMyPageTabLayout() = with(binding) {
         vpMeetUpContainer.adapter = MeetUpDetailVpAdapter(this@MeetUpContainerFragment)
+
+        val tabTitleArray = arrayOf(
+            MEETUP_INFO,
+            READY_STATUS,
+            LATE_PERSON,
+        )
+
+        TabLayoutMediator(tabMeetUpContainer, vpMeetUpContainer) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
+
         vpMeetUpContainer.setUserInputEnabled(false)
+    }
+
+    companion object {
+        private const val MEETUP_INFO = "약속 정보"
+        private const val READY_STATUS = "준비 현황"
+        private const val LATE_PERSON = "지각 꾸물이"
     }
 }
