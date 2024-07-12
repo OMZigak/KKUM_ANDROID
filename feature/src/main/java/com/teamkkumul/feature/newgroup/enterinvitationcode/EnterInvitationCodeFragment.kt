0d0ff42afeeda2bfbfd621ff38c69af5960daf1b
yskim6772 <yskim6772@gmail.com>
2024-07-12 19:55:03 +0900

    #47 [feat] : 모임 초대 코드 최대 6자 + 6자일 때만 버튼 활성화 구현

--- feature/src/main/java/com/teamkkumul/feature/newgroup/enterinvitationcode/EnterInvitationCodeFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/enterinvitationcode/EnterInvitationCodeFragment.kt
@@ -1,12 +1,71 @@
 package com.teamkkumul.feature.newgroup.enterinvitationcode
 
+import android.text.Editable
+import android.text.TextWatcher
 import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.core.ui.util.fragment.colorOf
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentEnterInvitationCodeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class EnterInvitationCodeFragment : BindingFragment<FragmentEnterInvitationCodeBinding>(R.layout.fragment_enter_invitation_code) {
+class EnterInvitationCodeFragment :
+    BindingFragment<FragmentEnterInvitationCodeBinding>(R.layout.fragment_enter_invitation_code) {
+
+    private var currentText: String = ""
+
     override fun initView() {
+        setInvitationCode()
+        binding.btnNext.setOnClickListener {
+            val input = binding.etEnterInvitationCode.text.toString()
+            validInput(input)
+        }
+
+        binding.etEnterInvitationCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val input = s.toString()
+                updateButtonState(input.length == 6)
+            }
+        })
+    }
+
+    private fun setInvitationCode() = with(binding.etEnterInvitationCode) {
+        setOnEditorActionListener { _, actionId, event ->
+            (actionId == android.view.inputmethod.EditorInfo.IME_ACTION_DONE || (event != null && event.keyCode == android.view.KeyEvent.KEYCODE_ENTER))
+        }
+    }
+
+    private fun validInput(input: String) {
+        val isValid = input.length == 6
+        if (isValid) {
+            currentText = input
+            setErrorState(null)
+        } else {
+            setColor(R.color.red)
+            setErrorState(getString(R.string.set_name_error_message))
+        }
+        updateButtonState(isValid)
+    }
+
+    private fun setErrorState(errorMessage: String?) {
+        with(binding) {
+            tilEnterInvitationCode.error = errorMessage
+            tilEnterInvitationCode.isErrorEnabled = errorMessage != null
+        }
+    }
+
+    private fun setColor(colorResId: Int) {
+        val color = colorOf(colorResId)
+        with(binding) {
+            tilEnterInvitationCode.boxStrokeColor = color
+        }
+    }
+
+    private fun updateButtonState(isValid: Boolean) {
+        binding.btnNext.isEnabled = isValid
     }
 }
