aa7ea0aec08db48c11dd3be8e0e60ea0d1bef100
yskim6772 <yskim6772@gmail.com>
2024-07-13 06:33:32 +0900

    #48 [feat] : splash activity 0.2초로 구현, 디폴트 앱 아이콘 splash 삭제

---
+++ feature/src/main/java/com/teamkkumul/feature/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.teamkkumul.feature.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import com.teamkkumul.core.ui.base.BindingActivity
+import com.teamkkumul.core.ui.util.context.statusBarColorOf
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.auth.LoginActivity
+import com.teamkkumul.feature.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun initView() {
+        statusBarColorOf(R.color.main_color)
+        activityScope.launch {
+            delay(300)
+
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+}
