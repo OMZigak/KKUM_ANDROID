777ecc79f2cf7003c008ddefeb85c84ae09c65d8
yskim6772 <yskim6772@gmail.com>
2024-07-15 02:43:46 +0900

    #48 [refactor] : 라이프 사이클 스코프 사용

--- feature/src/main/java/com/teamkkumul/feature/splash/SplashActivity.kt
+++ feature/src/main/java/com/teamkkumul/feature/splash/SplashActivity.kt
@@ -2,35 +2,32 @@ package com.teamkkumul.feature.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.teamkkumul.core.ui.base.BindingActivity
 import com.teamkkumul.core.ui.util.context.statusBarColorOf
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.auth.LoginActivity
 import com.teamkkumul.feature.databinding.ActivitySplashBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
-    private val activityScope = CoroutineScope(Dispatchers.Main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
 
     override fun initView() {
         statusBarColorOf(R.color.main_color)
-        activityScope.launch {
+        lifecycleScope.launch {
             delay(1000)
 
             val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
-    }
-
-    override fun onPause() {
-        activityScope.cancel()
-        super.onPause()
     }
 }
