9d3764c5bf5eadf6cfe96b3e3465344eabb0de78
chanu <cw8881@naver.com>
2024-07-15 17:10:32 +0900

    #41 [feat] : 준비 현황 화면 구현

--- feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/ReadyStatusFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/ReadyStatusFragment.kt
@@ -1,11 +1,141 @@
 package com.teamkkumul.feature.meetup.readystatus
 
+import android.widget.ImageView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.core.ui.util.fragment.colorOf
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentReadyStatusBinding
+import com.teamkkumul.feature.meetup.readystatus.viewholder.ReadyStatusFriendItemDecoration
+import com.teamkkumul.feature.utils.PROGRESS.PROGRESS_NUM_100
+import com.teamkkumul.feature.utils.PROGRESS.PROGRESS_TIME
+import com.teamkkumul.feature.utils.animateProgressBar
+import com.teamkkumul.feature.utils.getCurrentTime
+import com.teamkkumul.feature.utils.model.BtnState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class ReadyStatusFragment :
     BindingFragment<FragmentReadyStatusBinding>(R.layout.fragment_ready_status) {
+    private val viewModel: ReadyStatusViewModel by viewModels()
+
+    private var _readyStatusAdapter: ReadyStatusAdapter? = null
+    private val readyStatusAdapter get() = requireNotNull(_readyStatusAdapter)
+
     override fun initView() {
+        initReadyStatusBtnClick()
+        initObserveBtnState()
+        initReadyStatusRecyclerview()
+        initReadyInputBtnClick()
+    }
+
+    private fun initReadyInputBtnClick() {
+        binding.tvReadyInfoNext.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_meet_up_container_to_readyInfoInputFragment)
+        }
+    }
+
+    private fun initReadyStatusRecyclerview() {
+        _readyStatusAdapter = ReadyStatusAdapter().apply {
+            submitList(viewModel.mockMembers)
+        }
+        binding.rvReadyStatusFriendList.apply {
+            adapter = readyStatusAdapter
+            addItemDecoration(ReadyStatusFriendItemDecoration(requireContext()))
+        }
+    }
+
+    private fun initReadyStatusBtnClick() {
+        initReadyBtnClick()
+        initMovingBtnClick()
+        initArriveBtnClick()
+    }
+
+    private fun initReadyBtnClick() = with(binding) {
+        btnHomeReady.setOnClickListener {
+            viewModel.clickReadyBtn()
+            tvHomeReadyTime.text = getCurrentTime()
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeReady, PROGRESS_NUM_100)
+            }
+        }
+    }
+
+    private fun initMovingBtnClick() = with(binding) {
+        btnHomeMoving.setOnClickListener {
+            viewModel.clickMovingStartBtn()
+            tvHomeMovingTime.text = getCurrentTime()
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeMoving, PROGRESS_NUM_100)
+            }
+        }
+    }
+
+    private fun initArriveBtnClick() = with(binding) {
+        btnHomeArrive.setOnClickListener {
+            viewModel.clickCompletedBtn()
+            tvvHomeArriveTime.text = getCurrentTime()
+
+            viewLifeCycleScope.launch {
+                animateProgressBar(pgHomeArrive, PROGRESS_NUM_100)
+                delay(PROGRESS_TIME)
+                animateProgressBar(pgHomeArriveEnd, PROGRESS_NUM_100)
+            }
+        }
+    }
+
+    private fun initObserveBtnState() = with(binding) {
+        observeBtnState(
+            viewModel.readyBtnState,
+            btnHomeReady,
+            ivHomeReadyCircle,
+        )
+        observeBtnState(
+            viewModel.movingStartBtnState,
+            binding.btnHomeMoving,
+            ivHomeMovingCircle,
+        )
+        observeBtnState(
+            viewModel.completedBtnState,
+            binding.btnHomeArrive,
+            ivHomeArriveCircle,
+        )
+    }
+
+    private fun observeBtnState(
+        stateFlow: StateFlow<BtnState>,
+        button: MaterialButton,
+        circle: ImageView,
+    ) {
+        stateFlow.flowWithLifecycle(viewLifeCycle).onEach { state ->
+            setUpButton(state, button, circle)
+        }.launchIn(viewLifeCycleScope)
+    }
+
+    private fun setUpButton(
+        state: BtnState,
+        button: MaterialButton,
+        circle: ImageView,
+    ) {
+        button.apply {
+            setStrokeColorResource(state.strokeColor)
+            setTextColor(colorOf(state.textColor))
+            setBackgroundColor(colorOf(state.backGroundColor))
+            isEnabled = state.isEnabled
+        }
+        circle.setImageResource(state.circleImage)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _readyStatusAdapter = null
     }
 }
---
+++ feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/ReadyStatusViewModel.kt
@@ -0,0 +1,100 @@
+package com.teamkkumul.feature.meetup.readystatus
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamkkumul.core.ui.view.UiState
+import com.teamkkumul.feature.utils.model.BtnState
+import com.teamkkumul.model.MeetUpDetailFriendModel
+import com.teamkkumul.model.MyGroupMeetUpModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ReadyStatusViewModel : ViewModel() {
+    private val _readyBtnState =
+        MutableStateFlow<BtnState>(BtnState.Default(isEnabled = true))
+    val readyBtnState: StateFlow<BtnState> get() = _readyBtnState
+
+    private val _movingStartBtnState =
+        MutableStateFlow<BtnState>(BtnState.DefaultGray(isEnabled = false))
+    val movingStartBtnState: StateFlow<BtnState> get() = _movingStartBtnState
+
+    private val _completedBtnState =
+        MutableStateFlow<BtnState>(BtnState.DefaultGray(isEnabled = false))
+    val completedBtnState: StateFlow<BtnState> get() = _completedBtnState
+
+    private val _homePromiseState =
+        MutableStateFlow<UiState<List<MyGroupMeetUpModel.Promise>>>(UiState.Loading)
+    val homePromiseState get() = _homePromiseState.asStateFlow()
+
+    fun clickReadyBtn() {
+        viewModelScope.launch {
+            if (isCompleteState(_readyBtnState)) return@launch
+            _readyBtnState.emit(BtnState.InProgress(isEnabled = false))
+            _movingStartBtnState.emit(BtnState.Default(isEnabled = true))
+            _completedBtnState.emit(BtnState.DefaultGray(isEnabled = false))
+        }
+    }
+
+    fun clickMovingStartBtn() {
+        viewModelScope.launch {
+            if (isCompleteState(_movingStartBtnState)) return@launch
+            _readyBtnState.emit(BtnState.Complete(isEnabled = false))
+            _movingStartBtnState.emit(BtnState.InProgress(isEnabled = false))
+            _completedBtnState.emit(BtnState.Default(isEnabled = true))
+        }
+    }
+
+    fun clickCompletedBtn() {
+        viewModelScope.launch {
+            if (isCompleteState(_completedBtnState)) return@launch
+            _readyBtnState.emit(BtnState.Complete(isEnabled = false))
+            _movingStartBtnState.emit(BtnState.Complete(isEnabled = false))
+            _completedBtnState.emit(BtnState.Complete(isEnabled = false))
+        }
+    }
+
+    private fun isCompleteState(stateFlow: StateFlow<BtnState>): Boolean {
+        return stateFlow.value is BtnState.Complete
+    }
+
+    val mockMembers = listOf(
+        MeetUpDetailFriendModel.Participant(
+            id = 2,
+            name = "Eric",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+        MeetUpDetailFriendModel.Participant(
+            id = 3,
+            name = "Bob",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+        MeetUpDetailFriendModel.Participant(
+            id = 2,
+            name = "Alice",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+        MeetUpDetailFriendModel.Participant(
+            id = 2,
+            name = "Eric",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+        MeetUpDetailFriendModel.Participant(
+            id = 3,
+            name = "Bob",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+        MeetUpDetailFriendModel.Participant(
+            id = 2,
+            name = "Alice",
+            profileImg = "https://example.com/alice.jpg",
+            state = "이동중",
+        ),
+    )
+}
