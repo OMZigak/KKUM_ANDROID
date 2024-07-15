d40d046de1d059459a0bcbc802a9613b0ec16dd2
chanu <cw8881@naver.com>
2024-07-15 18:42:17 +0900

    #41 [ui] : 준비 정보 입력 완료 화며 구현

---
+++ feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/complete/ReadyInputCompletedFragment.kt
@@ -0,0 +1,11 @@
+package com.teamkkumul.feature.meetup.readystatus.complete
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentReadyInputCompletedBinding
+
+class ReadyInputCompletedFragment :
+    BindingFragment<FragmentReadyInputCompletedBinding>(R.layout.fragment_ready_input_completed) {
+    override fun initView() {
+    }
+}
