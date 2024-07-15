6835d772fae400d01b998b51e24950a339d86d0c
yskim6772 <yskim6772@gmail.com>
2024-07-16 05:08:37 +0900

    #56 [feat] : late person 뷰모델 적용

--- feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonFragment.kt
@@ -1,21 +1,25 @@
 package com.teamkkumul.feature.meetup.lateperson
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.teamkkumul.core.ui.base.BindingFragment
 import com.teamkkumul.core.ui.util.context.pxToDp
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentLatePersonBinding
 import com.teamkkumul.feature.utils.itemdecorator.GridSpacingItemDecoration
-import com.teamkkumul.model.LatePerson
+import kotlinx.coroutines.launch
 
 class LatePersonFragment :
     BindingFragment<FragmentLatePersonBinding>(R.layout.fragment_late_person) {
+
+    private val viewModel: LatePersonViewModel by viewModels()
 
     private var _latePersonAdapter: LatePersonAdapter? = null
     private val latePersonAdapter get() = requireNotNull(_latePersonAdapter)
     override fun initView() {
         initRecyclerView()
-        loadData()
+        observeViewModel()
     }
 
     private fun initRecyclerView() {
@@ -27,14 +31,11 @@ class LatePersonFragment :
         }
     }
 
-    private fun loadData() {
-        val latePersons = listOf(
-            LatePerson(1, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePerson(2, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePerson(3, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePerson(4, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePerson(5, "혜진 티엘", "https://example.com/image1.jpg"),
-        )
-        latePersonAdapter.submitList(latePersons)
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.latePerson.collect { latePerson ->
+                latePersonAdapter.submitList(latePerson)
+            }
+        }
     }
 }
