625b81b51b89bd9000470300e64c7cf7e5ee2a4e
yskim6772 <yskim6772@gmail.com>
2024-07-16 04:50:21 +0900

    #56 [feat] : 지각 꾸물이 화면 UI 구현

--- feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonFragment.kt
@@ -1,11 +1,40 @@
 package com.teamkkumul.feature.meetup.lateperson
 
+import androidx.recyclerview.widget.GridLayoutManager
 import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.core.ui.util.context.pxToDp
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentLatePersonBinding
+import com.teamkkumul.feature.utils.itemdecorator.GridSpacingItemDecoration
+import com.teamkkumul.model.LatePerson
 
 class LatePersonFragment :
     BindingFragment<FragmentLatePersonBinding>(R.layout.fragment_late_person) {
+
+    private var _latePersonAdapter: LatePersonAdapter? = null
+    private val latePersonAdapter get() = requireNotNull(_latePersonAdapter)
     override fun initView() {
+        initRecyclerView()
+        loadData()
+    }
+
+    private fun initRecyclerView() {
+        _latePersonAdapter = LatePersonAdapter()
+        binding.rvLatePerson.apply {
+            layoutManager = GridLayoutManager(requireContext(), 3)
+            adapter = latePersonAdapter
+            addItemDecoration(GridSpacingItemDecoration(3, requireContext().pxToDp(8)))
+        }
+    }
+
+    private fun loadData() {
+        val latePersons = listOf(
+            LatePerson(1, "혜진 티엘", "https://example.com/image1.jpg"),
+            LatePerson(2, "혜진 티엘", "https://example.com/image1.jpg"),
+            LatePerson(3, "혜진 티엘", "https://example.com/image1.jpg"),
+            LatePerson(4, "혜진 티엘", "https://example.com/image1.jpg"),
+            LatePerson(5, "혜진 티엘", "https://example.com/image1.jpg"),
+        )
+        latePersonAdapter.submitList(latePersons)
     }
 }
---
+++ feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonViewHolder.kt
@@ -0,0 +1,17 @@
+package com.teamkkumul.feature.meetup.lateperson
+
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.teamkkumul.feature.databinding.ItemMeetUpCreateFriendBinding
+import com.teamkkumul.model.LatePerson
+
+class LatePersonViewHolder(
+    private val binding: ItemMeetUpCreateFriendBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(latePerson: LatePerson) {
+        with(binding) {
+            ivMeetUpCreateFriendProfileImage.load(latePerson.profileImg)
+            tvMyGroupCreateFrinedProfileName.text = latePerson.name
+        }
+    }
+}
