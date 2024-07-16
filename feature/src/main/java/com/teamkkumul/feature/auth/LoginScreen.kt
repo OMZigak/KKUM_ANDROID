f9b8941e6b0449d7d80a6af3f5ff0824d66b27b5
chanu <cw8881@naver.com>
2024-07-17 01:11:40 +0900

    #55 [feat] : 로그인 화면 ui 수정

--- feature/src/main/java/com/teamkkumul/feature/auth/LoginScreen.kt
+++ feature/src/main/java/com/teamkkumul/feature/auth/LoginScreen.kt
@@ -1,6 +1,10 @@
 package com.teamkkumul.feature.auth
 
+import android.Manifest
 import android.app.Activity
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -31,16 +35,21 @@ import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.messaging.FirebaseMessaging
 import com.teamkkumul.core.designsystem.theme.Gray8
 import com.teamkkumul.core.designsystem.theme.Green4
 import com.teamkkumul.core.designsystem.theme.KkumulAndroidTheme
 import com.teamkkumul.core.designsystem.theme.KkumulTheme
 import com.teamkkumul.core.designsystem.theme.Yellow
 import com.teamkkumul.core.ui.util.context.toast
+import com.teamkkumul.core.ui.util.intent.navigateTo
+import com.teamkkumul.feature.MainActivity
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.auth.model.LoginSideEffect
+import com.teamkkumul.feature.signup.SetNameActivity
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
 @Composable
 fun LoginRoute(
@@ -49,8 +58,31 @@ fun LoginRoute(
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
 
+    // 권한 요청 런처 초기화
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                // 권한이 허용된 경우 FCM 토큰을 받아옴
+                handlePushAlarmPermissionGranted { token ->
+                    viewModel.setFcmToken(token)
+                    Timber.d("fcm token: $token")
+                }
+            }
+        },
+    )
+
+    // 컴포지션이 시작될 때 권한을 요청
     LaunchedEffect(Unit) {
-        viewModel.getUserToken()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Tiramisu 버전 이상에서 POST_NOTIFICATIONS 권한 요청
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            // Tiramisu 버전 이하에서는 바로 FCM 토큰을 받아옴
+            handlePushAlarmPermissionGranted { token ->
+                viewModel.setFcmToken(token)
+            }
+        }
     }
 
     LaunchedEffect(lifecycleOwner) {
@@ -58,7 +90,8 @@ fun LoginRoute(
             .onEach { sideEffect ->
                 when (sideEffect) {
                     is LoginSideEffect.ShowSnackBar -> context.toast(sideEffect.message)
-                    else -> {}
+                    is LoginSideEffect.NavigateToMain -> navigateTo<MainActivity>(context)
+                    is LoginSideEffect.NavigateToOnBoarding -> navigateTo<SetNameActivity>(context)
                 }
             }.launchIn(lifecycleOwner.lifecycleScope)
     }
@@ -68,6 +101,17 @@ fun LoginRoute(
             viewModel.startKaKaoLogin(context)
         },
     )
+}
+
+private fun handlePushAlarmPermissionGranted(onTokenReceived: (String) -> Unit) {
+    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            val token = task.result
+            onTokenReceived(token)
+        } else {
+            Timber.d(task.exception)
+        }
+    }
 }
 
 @Composable
