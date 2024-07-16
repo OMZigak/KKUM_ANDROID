8404bd978f4125a7ef32ca8082c0774f187b7086
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-17 00:13:54 +0900

    [feat/#58] MyGroupFragment 서버통신

--- feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupFragment.kt
@@ -1,47 +1,76 @@
 package com.teamkkumul.feature.mygroup
 
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
 import com.teamkkumul.core.ui.view.UiState
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentMyGroupBinding
 import com.teamkkumul.feature.mygroup.adapter.MyGroupAdapter
+import com.teamkkumul.model.MyGroupModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class MyGroupFragment : BindingFragment<FragmentMyGroupBinding>(R.layout.fragment_my_group) {
-    private val myGroupViewModel: MyGroupViewModel by viewModels()
+    private val viewModel: MyGroupViewModel by viewModels()
     private var _memberAdapter: MyGroupAdapter? = null
     private val memberAdapter get() = requireNotNull(_memberAdapter)
 
     override fun initView() {
         initGroupRecyclerView()
-        initObserveGroupState()
+        initObserveUiState()
+        viewModel.getMyGroupList()
     }
 
-    private fun initObserveGroupState() {
-        myGroupViewModel.group.observe(viewLifecycleOwner) {
-            when (it) {
-                is UiState.Empty -> {
-                    binding.viewMyGroupEmpty.visibility = View.VISIBLE
-                    binding.rvMyGroupList.visibility = View.GONE
-                }
-
-                is UiState.Success -> {
-                    binding.rvMyGroupList.visibility = View.VISIBLE
-                    binding.viewMyGroupEmpty.visibility = View.GONE
-                    memberAdapter.submitList(it.data)
-                }
-
-                else -> {}
+    private fun initObserveUiState() {
+        viewModel.myGroupState.flowWithLifecycle(viewLifeCycle).onEach { uiState ->
+            when (uiState) {
+                is UiState.Empty -> emptyState()
+                is UiState.Loading -> loadingState()
+                is UiState.Success -> successState(uiState.data)
+                is UiState.Failure -> errorState(uiState.errorMessage)
             }
+        }.launchIn(viewLifeCycleScope)
+    }
+
+    private fun emptyState() {
+        with(binding) {
+            viewMyGroupEmpty.visibility = View.VISIBLE
+            rvMyGroupList.visibility = View.GONE
         }
     }
 
-    //    cl_my_group_plus
+    private fun loadingState() {
+        with(binding) {
+            viewMyGroupEmpty.visibility = View.GONE
+            rvMyGroupList.visibility = View.GONE
+        }
+    }
+
+    private fun successState(myGroupModel: MyGroupModel) {
+        with(binding) {
+            viewMyGroupEmpty.visibility = View.GONE
+            rvMyGroupList.visibility = View.VISIBLE
+        }
+        memberAdapter.submitList(myGroupModel.meetings)
+    }
+
+    private fun errorState(errorMessage: String) {
+        with(binding) {
+            viewMyGroupEmpty.visibility = View.VISIBLE
+            rvMyGroupList.visibility = View.GONE
+        }
+        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
     private fun initGroupRecyclerView() {
         binding.clMyGroupPlus.setOnClickListener {
             findNavController().navigate(R.id.action_myGroupFragment_to_addMyGroupFragment)
--- feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupViewModel.kt
@@ -1,34 +1,36 @@
 package com.teamkkumul.feature.mygroup
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamkkumul.core.data.repository.MyGroupRepository
 import com.teamkkumul.core.ui.view.UiState
 import com.teamkkumul.model.MyGroupModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MyGroupViewModel @Inject constructor() : ViewModel() {
+class MyGroupViewModel @Inject constructor(
+    private val myGroupRepository: MyGroupRepository,
+) : ViewModel() {
 
-    private val _group: MutableLiveData<UiState<List<MyGroupModel.Meeting>>> =
-        MutableLiveData(UiState.Loading)
-    val group: LiveData<UiState<List<MyGroupModel.Meeting>>> get() = _group
-    fun getPromise() {
-        viewModelScope.launch {
-            _group.value = UiState.Empty
-        }
-    }
+    private val _myGroupState = MutableStateFlow<UiState<MyGroupModel>>(UiState.Loading)
+    val myGroupState get() = _myGroupState.asStateFlow()
 
-    //    init {
-//        getPromise()
-//    }
-    init {
-        val mockGroups = listOf(
-            MyGroupModel.Meeting(id = 3, 3, "모각작"),
-        )
-        _group.value = UiState.Success(mockGroups)
+    fun getMyGroupList() = viewModelScope.launch {
+        _myGroupState.emit(UiState.Loading)
+        myGroupRepository.getMyGroupList()
+            .onSuccess { myGroupModel ->
+                if (myGroupModel.meetings.isEmpty()) {
+                    _myGroupState.emit(UiState.Empty)
+                } else {
+                    _myGroupState.emit(UiState.Success(myGroupModel))
+                }
+            }
+            .onFailure { exception ->
+                _myGroupState.emit(UiState.Failure(exception.message.toString()))
+            }
     }
 }
