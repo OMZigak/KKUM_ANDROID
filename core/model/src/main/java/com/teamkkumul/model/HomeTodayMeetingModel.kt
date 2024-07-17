434586e74d6d2a066c56939cffee2dce5e393dde
chanu <cw8881@naver.com>
2024-07-18 01:31:21 +0900

    #63 [add] : HomeTodayMeetingModel 추가

---
+++ core/model/src/main/java/com/teamkkumul/model/HomeTodayMeetingModel.kt
@@ -0,0 +1,12 @@
+package com.teamkkumul.model
+
+data class HomeTodayMeetingModel(
+    val dDay: Int,
+    val date: String,
+    val dressUpLevel: String,
+    val meetingName: String,
+    val name: String,
+    val placeName: String,
+    val promiseId: Int,
+    val time: String,
+)
