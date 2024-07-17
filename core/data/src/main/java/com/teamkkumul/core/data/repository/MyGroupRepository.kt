7b29202f0a10253a5b15b63703b1f05f6d904f24
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-18 05:19:11 +0900

    [feat/#64] MyGroupRepository, RepositoryImpl 구현

--- core/data/src/main/java/com/teamkkumul/core/data/repository/MyGroupRepository.kt
+++ core/data/src/main/java/com/teamkkumul/core/data/repository/MyGroupRepository.kt
@@ -1,7 +1,20 @@
 package com.teamkkumul.core.data.repository
 
+import com.teamkkumul.model.MyGroupDetailMemeberSealedItem
+import com.teamkkumul.model.MyGroupInfoModel
+import com.teamkkumul.model.MyGroupMeetUpModel
+import com.teamkkumul.model.MyGroupMemberModel
 import com.teamkkumul.model.MyGroupModel
 
 interface MyGroupRepository {
     suspend fun getMyGroup(): Result<MyGroupModel>
+    suspend fun getMyGroupMeetUp(
+        meetingId: Int,
+        done: Boolean,
+    ): Result<List<MyGroupMeetUpModel.Promise>>
+
+    suspend fun getMyGroupMember(meetingId: Int): Result<MyGroupMemberModel>
+
+    suspend fun getMyGroupMemberList(meetingId: Int): Result<List<MyGroupDetailMemeberSealedItem>>
+    suspend fun getMyGroupInfo(meetingId: Int): Result<MyGroupInfoModel>
 }
