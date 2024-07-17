f3d384679886960d1a7ee69ed4c60c5fb851275c
chanu <cw8881@naver.com>
2024-07-18 05:26:11 +0900

    #69 [fix] : 홈 다가올 약속 어뎁터 모델 수정

--- feature/src/main/java/com/teamkkumul/feature/home/HomeMeetUpAdapter.kt
+++ feature/src/main/java/com/teamkkumul/feature/home/HomeMeetUpAdapter.kt
@@ -6,15 +6,15 @@ import androidx.recyclerview.widget.ListAdapter
 import com.teamkkumul.core.ui.view.ItemDiffCallback
 import com.teamkkumul.feature.databinding.ItemMyGroupRemainMeetUpBinding
 import com.teamkkumul.feature.home.viewholder.HomeMeetUpViewHolder
-import com.teamkkumul.model.MyGroupMeetUpModel
+import com.teamkkumul.model.home.HomeTodayMeetingModel
 
 class HomeMeetUpAdapter(
-    private val onMeetUpDetailBtnClicked: () -> Unit,
-) : ListAdapter<MyGroupMeetUpModel.Promise, HomeMeetUpViewHolder>(diffUtil) {
+    private val onItemClicked: (Int) -> Unit,
+) : ListAdapter<HomeTodayMeetingModel, HomeMeetUpViewHolder>(diffUtil) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeMeetUpViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemMyGroupRemainMeetUpBinding.inflate(inflater, parent, false)
-        return HomeMeetUpViewHolder(binding, onMeetUpDetailBtnClicked)
+        return HomeMeetUpViewHolder(binding, onItemClicked)
     }
 
     override fun onBindViewHolder(holder: HomeMeetUpViewHolder, position: Int) {
@@ -22,8 +22,8 @@ class HomeMeetUpAdapter(
     }
 
     companion object {
-        private val diffUtil = ItemDiffCallback<MyGroupMeetUpModel.Promise>(
-            onItemsTheSame = { old, new -> old.date == new.date },
+        private val diffUtil = ItemDiffCallback<HomeTodayMeetingModel>(
+            onItemsTheSame = { old, new -> old.promiseId == new.promiseId },
             onContentsTheSame = { old, new -> old == new },
         )
     }
