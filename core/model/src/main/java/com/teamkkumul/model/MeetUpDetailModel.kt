cecbefbb110ab3495a28bdd6384c5c3a0ab0aba9
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-17 20:56:37 +0900

    [feat/#58] MeetUpDetailFragment api 구현

--- core/model/src/main/java/com/teamkkumul/model/MeetUpDetailModel.kt
+++ core/model/src/main/java/com/teamkkumul/model/MeetUpDetailModel.kt
@@ -1,10 +1,10 @@
 package com.teamkkumul.model
 
-data class MeetUpDetailModel(
-    val address: String,
+data class  MeetUpDetailModel(
+    val address: String?,
     val dressUpLevel: String,
     val penalty: String,
     val placeName: String,
-    val roadAddress: String,
+    val roadAddress: String?,
     val time: String,
 )
--- feature/src/main/java/com/teamkkumul/feature/meetup/meetupdetail/MeetUpDetailFriendViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/meetupdetail/MeetUpDetailFriendViewModel.kt
@@ -1,47 +1,60 @@
 package com.teamkkumul.feature.meetup.meetupdetail
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamkkumul.core.data.repository.MeetUpRepository
+import com.teamkkumul.core.ui.view.UiState
+import com.teamkkumul.model.MeetUpDetailModel
+import com.teamkkumul.model.MeetUpParticipantModel
 import com.teamkkumul.model.MeetUpSealedItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MeetUpDetailFriendViewModel : ViewModel() {
-    private val _members = MutableLiveData<List<MeetUpSealedItem.Participant>>()
-    val members: LiveData<List<MeetUpSealedItem.Participant>> get() = _members
+@HiltViewModel
+class MeetUpDetailFriendViewModel @Inject constructor(
+    private val meetUpRepository: MeetUpRepository,
+) : ViewModel() {
+    private val _meetUpDetailState = MutableStateFlow<UiState<MeetUpDetailModel>>(UiState.Loading)
+    val meetupDetailState get() = _meetUpDetailState.asStateFlow()
 
-    init {
-        val mockMembers = listOf(
-            MeetUpSealedItem.Participant(
-                id = 2,
-                name = "Eric",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-            MeetUpSealedItem.Participant(
-                id = 3,
-                name = "Bob",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-            MeetUpSealedItem.Participant(
-                id = 2,
-                name = "Alice",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-            MeetUpSealedItem.Participant(
-                id = 2,
-                name = "Eric",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-            MeetUpSealedItem.Participant(
-                id = 3,
-                name = "Bob",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-            MeetUpSealedItem.Participant(
-                id = 2,
-                name = "Alice",
-                profileImg = "https://example.com/alice.jpg",
-            ),
-        )
-        _members.value = mockMembers
+    private val _meetUpParticipantState =
+        MutableStateFlow<UiState<MeetUpParticipantModel>>(UiState.Loading)
+    val meetUpParticipantState get() = _meetUpParticipantState.asStateFlow()
+
+    private val _meetUpParticipantListState =
+        MutableStateFlow<UiState<List<MeetUpSealedItem>>>(UiState.Loading)
+    val meetUpParticipantListState get() = _meetUpParticipantListState.asStateFlow()
+
+    fun getMeetUpDetail(promiseId: Int) = viewModelScope.launch {
+        meetUpRepository.getMeetUpDetail(promiseId)
+            .onSuccess { meetUpModel ->
+                _meetUpDetailState.emit(UiState.Success(meetUpModel))
+            }
+            .onFailure { exception ->
+                _meetUpDetailState.emit(UiState.Failure(exception.message.toString()))
+            }
+    }
+
+    fun getMeetUpParticipant(promiseId: Int) = viewModelScope.launch {
+        meetUpRepository.getMeetUpParticipant(promiseId)
+            .onSuccess { meetUpParticipantModel ->
+                _meetUpParticipantState.emit(UiState.Success(meetUpParticipantModel))
+            }.onFailure { exception ->
+                _meetUpParticipantState.emit(UiState.Failure(exception.message.toString()))
+            }
+    }
+
+    fun getMeetUpParticipantList(promiseId: Int) {
+        viewModelScope.launch {
+            meetUpRepository.getMeetUpFriendList(promiseId)
+                .onSuccess {
+                    _meetUpParticipantListState.emit(UiState.Success(it))
+                }.onFailure { exception ->
+                    _meetUpParticipantListState.emit(UiState.Failure(exception.message.toString()))
+                }
+        }
     }
 }
