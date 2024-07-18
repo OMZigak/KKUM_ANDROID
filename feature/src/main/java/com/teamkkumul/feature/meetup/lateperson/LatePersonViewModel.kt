7538bf0325762412ee8e2156e557ecd5b70d24e2
yskim6772 <yskim6772@gmail.com>
2024-07-18 19:40:37 +0900

    #70 [chore] : dummy data 제거, 뷰모델 함수 수정

--- feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/lateperson/LatePersonViewModel.kt
@@ -2,38 +2,31 @@ package com.teamkkumul.feature.meetup.lateperson
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamkkumul.core.data.repository.MeetUpRepository
+import com.teamkkumul.core.ui.view.UiState
 import com.teamkkumul.model.LatePersonModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LatePersonViewModel : ViewModel() {
+@HiltViewModel
+class LatePersonViewModel @Inject constructor(
+    private val meetUpRepository: MeetUpRepository,
+) : ViewModel() {
 
-    private val _latePerson = MutableStateFlow<List<LatePersonModel.LateComers>>(emptyList())
-    val latePerson: StateFlow<List<LatePersonModel.LateComers>> = _latePerson
+    private val _latePersonState = MutableStateFlow<UiState<LatePersonModel>>(UiState.Loading)
+    val latePersonState get() = _latePersonState.asStateFlow()
 
-    init {
-        loadDummyData()
-    }
-
-    private fun loadDummyData() {
-        val dummyData = listOf(
-            LatePersonModel.LateComers(1, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(2, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(3, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(4, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(5, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(6, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(7, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(8, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(9, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(10, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(11, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(12, "혜진 티엘", "https://example.com/image1.jpg"),
-            LatePersonModel.LateComers(13, "혜진 티엘", "https://example.com/image1.jpg"),
-        )
+    fun getLateComersList(promiseId: Int) {
         viewModelScope.launch {
-            _latePerson.emit(dummyData)
+            meetUpRepository.getLateComersList(promiseId)
+                .onSuccess {
+                    _latePersonState.emit(UiState.Success(it))
+                }.onFailure { exception ->
+                    _latePersonState.emit(UiState.Failure(exception.message.toString()))
+                }
         }
     }
 }
