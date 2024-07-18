6574f0ee12a512b60760ce4f4133089bf07704be
chanu <cw8881@naver.com>
2024-07-18 22:46:52 +0900

    #72 [feat] : ready, patch servcie 함수 추가

--- core/network/src/main/java/com/teamkkumul/core/network/api/HomeService.kt
+++ core/network/src/main/java/com/teamkkumul/core/network/api/HomeService.kt
@@ -1,10 +1,13 @@
 package com.teamkkumul.core.network.api
 
 import com.teamkkumul.core.network.dto.response.BaseResponse
+import com.teamkkumul.core.network.dto.response.ResponseHomeReadyStatusDto
 import com.teamkkumul.core.network.dto.response.ResponseHomeUpComingMeetingDto
 import com.teamkkumul.core.network.dto.response.ResponseTodayMeetingDto
 import com.teamkkumul.core.network.dto.response.ResponseUserDto
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
 
 interface HomeService {
     @GET("api/v1/users/me")
@@ -13,6 +16,26 @@ interface HomeService {
     @GET("api/v1/promises/today/next")
     suspend fun getTodayMeeting(): BaseResponse<ResponseTodayMeetingDto>
 
-    @GET("/api/v1/promises/upcoming")
+    @GET("api/v1/promises/upcoming")
     suspend fun getUpComingMeeting(): BaseResponse<ResponseHomeUpComingMeetingDto>
+
+    @GET("api/v1/promises/{promiseId}/status")
+    suspend fun getReadyStatus(
+        @Path("promiseId") promiseId: Int,
+    ): BaseResponse<ResponseHomeReadyStatusDto?>
+
+    @PATCH("api/v1/promises/{promiseId}/preparation")
+    suspend fun patchReady(
+        @Path("promiseId") promiseId: Int,
+    ): BaseResponse<Unit>
+
+    @PATCH("api/v1/promises/{promiseId}/departure")
+    suspend fun patchMoving(
+        @Path("promiseId") promiseId: Int,
+    ): BaseResponse<Unit>
+
+    @PATCH("api/v1/promises/{promiseId}/arrival")
+    suspend fun patchCompleted(
+        @Path("promiseId") promiseId: Int,
+    ): BaseResponse<Unit>
 }
