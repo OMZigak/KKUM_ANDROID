3e599285e3b967ed32b5f7035703b64cd00ce67f
chanu <cw8881@naver.com>
2024-07-18 22:47:13 +0900

    #72 [feat] : 준비, 이동시작 버튼 repository 추가

--- core/data/src/main/java/com/teamkkumul/core/data/repositoryimpl/HomeRepositoryImpl.kt
+++ core/data/src/main/java/com/teamkkumul/core/data/repositoryimpl/HomeRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package com.teamkkumul.core.data.repositoryimpl
 
 import com.teamkkumul.core.data.mapper.toPromiseModel
+import com.teamkkumul.core.data.mapper.toReadyStatusModel
 import com.teamkkumul.core.data.mapper.toTodayMeetingModel
 import com.teamkkumul.core.data.mapper.toUserModel
 import com.teamkkumul.core.data.repository.HomeRepository
 import com.teamkkumul.core.network.api.HomeService
+import com.teamkkumul.model.home.HomeReadyStatusModel
 import com.teamkkumul.model.home.HomeTodayMeetingModel
 import com.teamkkumul.model.home.UserModel
 import javax.inject.Inject
@@ -23,5 +25,22 @@ internal class HomeRepositoryImpl @Inject constructor(
     override suspend fun getUpComingMeeting(): Result<List<HomeTodayMeetingModel>> = runCatching {
         homeService.getUpComingMeeting().data?.promises?.map { it.toPromiseModel() }
             ?: throw Exception("null")
+    }
+
+    override suspend fun getReadyStatus(promiseId: Int): Result<HomeReadyStatusModel?> =
+        runCatching {
+            homeService.getReadyStatus(promiseId).data?.toReadyStatusModel()
+        }
+
+    override suspend fun patchReady(promiseId: Int): Result<Unit> = runCatching {
+        homeService.patchReady(promiseId)
+    }
+
+    override suspend fun patchMoving(promiseId: Int): Result<Unit> = runCatching {
+        homeService.patchMoving(promiseId)
+    }
+
+    override suspend fun patchCompleted(promiseId: Int): Result<Unit> = runCatching {
+        homeService.patchCompleted(promiseId)
     }
 }
