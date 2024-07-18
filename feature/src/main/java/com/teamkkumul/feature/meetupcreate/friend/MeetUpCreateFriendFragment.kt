912b580e8d5a361335391c237064ab08ab109e50
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-19 04:59:13 +0900

    [feat/#85] MeetUpCreateFriendFragment 구현

--- feature/src/main/java/com/teamkkumul/feature/meetupcreate/friend/MeetUpCreateFriendFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetupcreate/friend/MeetUpCreateFriendFragment.kt
@@ -1,19 +1,29 @@
 package com.teamkkumul.feature.meetupcreate.friend
 
 import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.teamkkumul.core.ui.base.BindingFragment
 import com.teamkkumul.core.ui.util.context.pxToDp
 import com.teamkkumul.core.ui.util.fragment.toast
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
+import com.teamkkumul.core.ui.view.UiState
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentMeetUpFriendPlusBinding
 import com.teamkkumul.feature.meetupcreate.MeetUpCreateViewModel
+import com.teamkkumul.feature.utils.KeyStorage
+import com.teamkkumul.feature.utils.KeyStorage.MEETING_ID
 import com.teamkkumul.feature.utils.animateProgressBar
 import com.teamkkumul.feature.utils.itemdecorator.GridSpacingItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MeetUpCreateFriendFragment :
@@ -25,10 +35,13 @@ class MeetUpCreateFriendFragment :
     private val friendAdapter get() = requireNotNull(_friendAdapter)
 
     override fun initView() {
+        val id = arguments?.getInt(MEETING_ID) ?: -1
+
         viewModel.setProgressBar(50)
 
         initRecyclerView()
-        initObserveViewModel()
+        viewModel.getMyGroupMemberToMeetUp(id)
+        initObserveMyGroupMemberToMeetUp()
         initNextButton()
         observeProgress()
     }
@@ -62,11 +75,16 @@ class MeetUpCreateFriendFragment :
     }
 
     private fun updateNextButton(isEnabled: Boolean) {
+        val id = arguments?.getInt(MEETING_ID) ?: -1
+
         with(binding.tvMeetUpFriendPlusNext) {
             this.isEnabled = isEnabled
             if (isEnabled) {
                 setOnClickListener {
-                    findNavController().navigate(R.id.action_fragment_meet_up_create_friend_to_fragment_meet_up_level) // 이동할 페이지
+                    findNavController().navigate(
+                        R.id.action_fragment_meet_up_create_friend_to_fragment_meet_up_level,
+                        bundleOf(KeyStorage.MEETING_ID to id),
+                    )
                 }
                 ViewCompat.setBackgroundTintList(
                     this,
@@ -82,10 +100,18 @@ class MeetUpCreateFriendFragment :
         }
     }
 
-    private fun initObserveViewModel() {
-        viewModel.members.observe(viewLifecycleOwner) { members ->
-            friendAdapter.submitList(members)
-        }
+    private fun initObserveMyGroupMemberToMeetUp() {
+        viewModel.meetUpCreateMemberState.flowWithLifecycle(viewLifeCycle).onEach { uiState ->
+            when (uiState) {
+                is UiState.Success -> {
+                    friendAdapter.submitList(uiState.data)
+                    Timber.tag("dfdf").d(uiState.data.toString())
+                }
+
+                is UiState.Failure -> Timber.tag("meet up create friend").d(uiState.errorMessage)
+                else -> Unit
+            }
+        }.launchIn(viewLifeCycleScope)
     }
 
     private fun initNextButton() {
