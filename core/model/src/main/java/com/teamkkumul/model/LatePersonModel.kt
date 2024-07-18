a6067289dda38255788743c8e764e08eba34be2b
yskim6772 <yskim6772@gmail.com>
2024-07-18 19:39:54 +0900

    #70 [chore] : LatePersonModel isPastDue 타입 수정

--- core/model/src/main/java/com/teamkkumul/model/LatePersonModel.kt
+++ core/model/src/main/java/com/teamkkumul/model/LatePersonModel.kt
@@ -2,7 +2,7 @@ package com.teamkkumul.model
 
 data class LatePersonModel(
     val penalty: String,
-    val isPastDue: String,
+    val isPastDue: Boolean,
     val lateComers: List<LateComers>,
 ) {
     data class LateComers(
