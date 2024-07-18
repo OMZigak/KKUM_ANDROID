6f1d097ff265c99f5dbafdd6990f60f204f2d284
chanu <cw8881@naver.com>
2024-07-19 01:44:14 +0900

    #78 [chore] : model change

--- feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/viewholder/ReadyStatusViewHolder.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/viewholder/ReadyStatusViewHolder.kt
@@ -2,13 +2,14 @@ package com.teamkkumul.feature.meetup.readystatus.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import com.teamkkumul.feature.databinding.ItemReadyStatusBinding
-import com.teamkkumul.model.MeetUpDetailFriendModel
+import com.teamkkumul.feature.utils.setEmptyImageUrl
+import com.teamkkumul.model.home.HomeMembersStatus
 
 class ReadyStatusViewHolder(
     private val binding: ItemReadyStatusBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
-    private var item: MeetUpDetailFriendModel.Participant? = null
-    fun onBind(data: MeetUpDetailFriendModel.Participant) = with(binding) {
+    private var item: HomeMembersStatus.Participant? = null
+    fun onBind(data: HomeMembersStatus.Participant) = with(binding) {
         item = data
         participant = data
         executePendingBindings()
