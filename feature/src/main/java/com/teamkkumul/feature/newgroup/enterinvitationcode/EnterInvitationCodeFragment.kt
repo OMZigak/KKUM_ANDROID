cafcafee0f77c51cc877dc817ee97431a12938ba
yskim6772 <yskim6772@gmail.com>
2024-07-19 16:53:30 +0900

    #89 [feat] : 모임 가입 구현 수정 완료

--- feature/src/main/java/com/teamkkumul/feature/newgroup/enterinvitationcode/EnterInvitationCodeFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/newgroup/enterinvitationcode/EnterInvitationCodeFragment.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.core.os.bundleOf
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -21,7 +22,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -62,8 +62,7 @@ class EnterInvitationCodeFragment :
     }
 
     private fun observeViewModel() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            val id = arguments?.getInt(KeyStorage.MEETING_ID) ?: -1
+        val id = arguments?.getInt(KeyStorage.MEETING_ID) ?: -1
 //            viewModel.meetingsState.collect { state ->
 //                when (state) {
 //                    is UiState.Success -> {
@@ -81,26 +80,27 @@ class EnterInvitationCodeFragment :
 //                    else -> Unit
 //                }
 //            }
-            viewModel.meetingsState.flowWithLifecycle(viewLifeCycle).onEach {
-                when (it) {
+        viewModel.meetingsState
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
+            .onEach { state ->
+                when (state) {
                     is UiState.Success -> {
                         binding.ivInvitationCodeCheck.visibility = View.VISIBLE
                         delay(500L)
+                        val meetingId = state.data
                         findNavController().navigate(
                             R.id.action_fragment_enter_invitation_code_to_fragment_my_group_detail,
-                            bundleOf(MEETING_ID to id),
+                            bundleOf(MEETING_ID to meetingId),
                         )
-                        Timber.tag("첫번째보냄").d(id.toString())
+                        Timber.tag("첫번째보냄").d(meetingId.toString())
                     }
-
                     is UiState.Failure -> {
                         setErrorState(getString(R.string.set_enter_invitation_code_error_message))
                     }
-
                     else -> Unit
                 }
-            }.launchIn(viewLifeCycleScope)
-        }
+            }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     private fun initInvitationCode() {
