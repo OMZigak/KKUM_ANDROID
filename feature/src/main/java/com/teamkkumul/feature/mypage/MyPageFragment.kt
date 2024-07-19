9cbe734f511b3a433e575319718b17758a676f46
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-07-19 15:51:52 +0900

    [feat/#87] my page fragment 생성

--- feature/src/main/java/com/teamkkumul/feature/mypage/MyPageFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/mypage/MyPageFragment.kt
@@ -1,45 +1,60 @@
 package com.teamkkumul.feature.mypage
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
+import coil.load
 import com.teamkkumul.core.ui.base.BindingFragment
-import com.teamkkumul.core.ui.util.fragment.snackBar
 import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
 import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
 import com.teamkkumul.core.ui.view.UiState
 import com.teamkkumul.feature.R
 import com.teamkkumul.feature.databinding.FragmentMyPageBinding
+import com.teamkkumul.feature.utils.setEmptyImageUrl
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
     private val viewModel by viewModels<MyPageViewModel>()
+
     override fun initView() {
-        binding.btnMyPage.setOnClickListener {
-            viewModel.saveUserToken(binding.etMyPage.text.toString())
-        }
-
-        binding.btnGetToken.setOnClickListener {
-            viewModel.getUserToken()
-            initObserve()
-        }
-
-        binding.btnClear.setOnClickListener {
-            viewModel.deleteToken()
-        }
+        viewModel.getMyPageUserInfo()
+        initObserveMyPageState()
+        setSpanText()
     }
 
-    private fun initObserve() {
-        viewModel.userInfoState.flowWithLifecycle(viewLifeCycle).onEach {
+    private fun initObserveMyPageState() {
+        viewModel.myPageState.flowWithLifecycle(viewLifeCycle).onEach {
             when (it) {
-                is UiState.Success -> snackBar(binding.root, it.data)
-                is UiState.Failure -> snackBar(binding.root, it.errorMessage)
-                is UiState.Loading -> snackBar(binding.root, "Loading")
-                else -> {}
+                is UiState.Success -> binding.ivMyPageProfile.load(it.data.profileImg)
+                is UiState.Empty -> binding.ivMyPageProfile.setEmptyImageUrl("")
+                is UiState.Failure -> Timber.tag("my page").d(it.errorMessage)
+                else -> Unit
             }
         }.launchIn(viewLifeCycleScope)
+    }
+
+    private fun setSpanText() {
+        val textView = binding.tvMyPageUserState
+        val fullText = getString(R.string.my_page_user_state)
+        val spannableString = SpannableString(fullText)
+
+        if (fullText.length >= 5) {
+            spannableString.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.light_green)),
+                0,
+                5,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+            )
+        }
+
+        textView.text = spannableString
     }
 }
--- feature/src/main/java/com/teamkkumul/feature/mypage/MyPageViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/mypage/MyPageViewModel.kt
@@ -2,44 +2,35 @@ package com.teamkkumul.feature.mypage
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.teamkkumul.core.data.repository.UserInfoRepository
+import com.teamkkumul.core.data.repository.HomeRepository
 import com.teamkkumul.core.ui.view.UiState
+import com.teamkkumul.model.home.UserModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
-    private val userInfoRepository: UserInfoRepository,
+    private val homeRepository: HomeRepository,
 ) : ViewModel() {
-    private val _userInfoState = MutableStateFlow<UiState<String>>(UiState.Empty)
-    val userInfoState get() = _userInfoState.asStateFlow()
+    private val _myPageState = MutableStateFlow<UiState<UserModel>>(UiState.Loading)
+    val myPageState: StateFlow<UiState<UserModel>> = _myPageState.asStateFlow()
 
-    fun saveUserToken(token: String) {
+    fun getMyPageUserInfo() {
         viewModelScope.launch {
-            userInfoRepository.saveAccessToken(token)
-        }
-    }
-
-    fun getUserToken() {
-        viewModelScope.launch {
-            userInfoRepository.getAccessToken().collectLatest {
-                _userInfoState.emit(UiState.Loading)
-                if (it.isNotEmpty()) {
-                    _userInfoState.emit(UiState.Success(it))
-                } else {
-                    _userInfoState.emit(UiState.Failure(it))
+            homeRepository.getUserInfo()
+                .onSuccess {
+                    if (it.profileImg == null) {
+                        _myPageState.emit(UiState.Empty)
+                    } else {
+                        _myPageState.emit(UiState.Success(it))
+                    }
+                }.onFailure {
+                    _myPageState.emit(UiState.Failure(it.message.toString()))
                 }
-            }
-        }
-    }
-
-    fun deleteToken() {
-        viewModelScope.launch {
-            userInfoRepository.clearAll()
         }
     }
 }
