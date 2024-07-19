032b8586f71f117626fa5d5f7b537d4a2b391426
yskim6772 <yskim6772@gmail.com>
2024-07-19 11:40:21 +0900

    #89 [feat] : api service, dto 생성

--- core/network/src/main/java/com/teamkkumul/core/network/api/MeetUpCreateService.kt
+++ core/network/src/main/java/com/teamkkumul/core/network/api/MeetUpCreateService.kt
@@ -1,8 +1,13 @@
 package com.teamkkumul.core.network.api
 
+import com.teamkkumul.core.network.dto.request.RequestMeetUpCreateDto
 import com.teamkkumul.core.network.dto.response.BaseResponse
 import com.teamkkumul.core.network.dto.response.ResponseMeetUpCreateLocationDto
+import com.teamkkumul.core.network.dto.response.ResponseMeetUpDetailDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MeetUpCreateService {
@@ -10,4 +15,10 @@ interface MeetUpCreateService {
     suspend fun getMeetUpCreateLocation(
         @Query("q") q: String,
     ): BaseResponse<ResponseMeetUpCreateLocationDto>
+
+    @POST("/api/v1/meetings/{meetingId}/promises")
+    suspend fun postNewMeetUp(
+        @Path("meetingId") meetingId: Int,
+        @Body request: RequestMeetUpCreateDto,
+    ): BaseResponse<ResponseMeetUpDetailDto>
 }
