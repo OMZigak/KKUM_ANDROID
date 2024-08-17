4cd4521d43f5e4c22544832f0646919bc7739ac7
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-08-17 19:28:42 +0900

    [feat/#129] MyGroupFragment에서는 추가 버튼 안보이게 하기

--- feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/mygroup/MyGroupFragment.kt
@@ -33,6 +33,8 @@ class MyGroupFragment : BindingFragment<FragmentMyGroupBinding>(R.layout.fragmen
         viewModel.getMyGroupList()
         viewModel.getMyGroupCount()
         initGetName()
+
+        binding.toolbarMyGroup.ivBtnMore.visibility = View.GONE
     }
 
     private fun initGetName() {
