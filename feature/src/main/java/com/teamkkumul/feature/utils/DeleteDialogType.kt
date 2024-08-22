daa342ece70aa4ced1dd461dfc50e67496e883c6
chanu <cw8881@naver.com>
2024-08-22 21:09:00 +0900

    #133 [fix] : DeleteDialogType 수정

--- feature/src/main/java/com/teamkkumul/feature/utils/DeleteDialogType.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/DeleteDialogType.kt
@@ -1,27 +1,29 @@
 package com.teamkkumul.feature.utils
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.teamkkumul.feature.R
 
 enum class DeleteDialogType(
-    val question: String,
-    val questionDescription: String,
-    val imageResId: Int,
-    val btnText: String = R.string.tv_btn_leave.toString(),
+    @StringRes val question: Int,
+    @StringRes val questionDescription: Int,
+    @DrawableRes val imageResId: Int,
+    @StringRes val btnText: Int = R.string.tv_btn_leave,
 ) {
-    MEET_UP_LEAVE_DIALOG(
-        question = R.string.tv_leave_question.toString(),
-        questionDescription = R.string.tv_leave_question_description.toString(),
-        imageResId = R.drawable.ic_dialog_leave_meet_up,
-    ),
     MY_GROUP_LEAVE_DIALOG(
-        question = R.string.tv_leave_my_group_question.toString(),
-        questionDescription = R.string.tv_leave_my_group_question_description.toString(),
+        question = R.string.tv_leave_my_group_question,
+        questionDescription = R.string.tv_leave_my_group_question_description,
         imageResId = R.drawable.ic_dialog_leave_my_group,
     ),
-    MEET_UP_DELETE_DIALOG(
-        question = R.string.tv_delete_meet_up_question.toString(),
-        questionDescription = R.string.tv_delete_meet_up_question_description.toString(),
+    PROMISE_LEAVE_DIALOG(
+        question = R.string.tv_leave_question,
+        questionDescription = R.string.tv_leave_question_description,
+        imageResId = R.drawable.ic_dialog_leave_meet_up,
+    ),
+    PROMISE_DELETE_DIALOG(
+        question = R.string.tv_delete_meet_up_question,
+        questionDescription = R.string.tv_delete_meet_up_question_description,
         imageResId = R.drawable.ic_dialog_delete_meet_up,
-        btnText = R.string.tv_btn_delete.toString(),
+        btnText = R.string.tv_btn_delete,
     ),
 }
