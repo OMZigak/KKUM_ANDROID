80e821471f169c227d9bd97ee9a1dac56cb883d8
chanu <cw8881@naver.com>
2024-08-26 00:11:56 +0900

    #142 [feat] : date에 따른 색상 구분 확장함수 구현

--- feature/src/main/java/com/teamkkumul/feature/utils/time/setDday.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/time/setDday.kt
@@ -1,7 +1,21 @@
 package com.teamkkumul.feature.utils.time
 
+import com.teamkkumul.feature.R
+
 fun setDday(dDay: Int): String = when {
     dDay == 0 -> "D-DAY"
     dDay > 0 -> "D+$dDay"
     else -> "D$dDay"
 }
+
+fun setDdayTextColor(dDay: Int): Int = when {
+    dDay == 0 -> R.color.orange
+    dDay > 0 -> R.color.gray3
+    else -> R.color.gray5
+}
+
+fun setPromiseTextColor(dDay: Int): Int = when {
+    dDay > 0 -> R.color.gray3
+    else -> R.color.gray8
+}
+
