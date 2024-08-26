9dfafb2b1b62d06350cf332378bc6fc58700afa3
chanu <cw8881@naver.com>
2024-08-26 22:17:37 +0900

    #144 [feat] : level updateLevelText extension함수 추가, createSpannableLevelText 확장함수 추가

---
+++ feature/src/main/java/com/teamkkumul/feature/utils/extension/LevelExt.kt
@@ -0,0 +1,42 @@
+package com.teamkkumul.feature.utils.extension
+
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import com.teamkkumul.core.ui.util.context.colorOf
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.utils.type.LevelColorType
+
+fun Context.updateLevelText(level: Int, colorType: LevelColorType): SpannableString {
+    val levelText = getString(
+        when (level) {
+            1 -> R.string.home_lv1
+            2 -> R.string.home_lv2
+            3 -> R.string.home_lv3
+            4 -> R.string.home_lv4
+            else -> R.string.home_lv1
+        },
+    )
+
+    return createSpannableLevelText(level, levelText, colorType)
+}
+
+fun Context.createSpannableLevelText(
+    level: Int,
+    text: String,
+    colorType: LevelColorType,
+): SpannableString {
+    val fullText = getString(R.string.home_level_text, level, text)
+    val spannable = SpannableString(fullText)
+
+    // 레벨 텍스트에 전달받은 색상으로 스타일 적용
+    spannable.setSpan(
+        ForegroundColorSpan(colorOf(colorType.colorResId)),
+        0,
+        4,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+    )
+
+    return spannable
+}
