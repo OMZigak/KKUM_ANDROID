0e1f50a5f16d317f2088c9376ed8c9cc219d3322
chanu <cw8881@naver.com>
2024-08-26 20:04:00 +0900

    #144 [add] : logout, delete interface, impl 구현체 구현

--- core/data/src/main/java/com/teamkkumul/core/data/repository/LoginRepository.kt
+++ core/data/src/main/java/com/teamkkumul/core/data/repository/LoginRepository.kt
@@ -4,4 +4,8 @@ import com.teamkkumul.model.login.LoginModel
 
 interface LoginRepository {
     suspend fun postLogin(socialType: String, fcmToken: String, header: String): Result<LoginModel>
+
+    suspend fun postLogout(): Result<Unit>
+
+    suspend fun deleteWithdrawal(): Result<Unit>
 }
--- core/data/src/main/java/com/teamkkumul/core/data/repositoryimpl/LoginRepositoryImpl.kt
+++ core/data/src/main/java/com/teamkkumul/core/data/repositoryimpl/LoginRepositoryImpl.kt
@@ -4,12 +4,14 @@ import com.teamkkumul.core.data.mapper.toLoginModel
 import com.teamkkumul.core.data.repository.LoginRepository
 import com.teamkkumul.core.data.utils.handleThrowable
 import com.teamkkumul.core.network.api.LoginService
+import com.teamkkumul.core.network.api.LogoutService
 import com.teamkkumul.core.network.dto.request.RequestLoginDto
 import com.teamkkumul.model.login.LoginModel
 import javax.inject.Inject
 
 internal class LoginRepositoryImpl @Inject constructor(
     private val loginService: LoginService,
+    private val logoutService: LogoutService,
 ) : LoginRepository {
     override suspend fun postLogin(
         socialType: String,
@@ -25,4 +27,14 @@ internal class LoginRepositoryImpl @Inject constructor(
     }.recoverCatching {
         return it.handleThrowable()
     }
+
+    override suspend fun postLogout(): Result<Unit> = runCatching {
+        logoutService.postLogout()
+        Unit
+    }.onFailure { return it.handleThrowable() }
+
+    override suspend fun deleteWithdrawal(): Result<Unit> = runCatching {
+        logoutService.deleteWithdrawal()
+        Unit
+    }.onFailure { return it.handleThrowable() }
 }
