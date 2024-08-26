9c8748f7b5fa159996746ac80d402543ed1067bd
chanu <cw8881@naver.com>
2024-08-26 13:32:44 +0900

    #144 [feat] : dialog type 추가 정리

--- feature/src/main/java/com/teamkkumul/feature/utils/type/DeleteDialogType.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/type/DeleteDialogType.kt
@@ -7,8 +7,9 @@ import com.teamkkumul.feature.R
 enum class DeleteDialogType(
     @StringRes val question: Int,
     @StringRes val questionDescription: Int,
-    @DrawableRes val imageResId: Int,
-    @StringRes val btnText: Int = R.string.tv_btn_leave,
+    @DrawableRes val imageResId: Int = R.drawable.ic_dialog_leave_my_group,
+    @StringRes val btnLeftText: Int = R.string.tv_btn_cancel,
+    @StringRes val btnRightText: Int = R.string.tv_btn_leave,
 ) {
     MY_GROUP_LEAVE_DIALOG(
         question = R.string.tv_leave_my_group_question,
@@ -24,6 +25,18 @@ enum class DeleteDialogType(
         question = R.string.tv_delete_meet_up_question,
         questionDescription = R.string.tv_delete_meet_up_question_description,
         imageResId = R.drawable.ic_dialog_delete_meet_up,
-        btnText = R.string.tv_btn_delete,
+        btnRightText = R.string.tv_btn_delete,
+    ),
+    Logout(
+        question = R.string.dialog_logout_title,
+        questionDescription = R.string.dialog_logout_description,
+        btnLeftText = R.string.tv_btn_cancel,
+        btnRightText = R.string.dialog_logout_btn_right,
+    ),
+    Withdrawal(
+        question = R.string.tv_delete_meet_up_question,
+        questionDescription = R.string.tv_delete_meet_up_question_description,
+        btnLeftText = R.string.tv_btn_cancel,
+        btnRightText = R.string.dialog_withdrawal_btn_right,
     ),
 }
