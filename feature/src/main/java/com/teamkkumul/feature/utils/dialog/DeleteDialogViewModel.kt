a479cf4baaecbb1b14617b6dded66ee8783c85e2
chanu <cw8881@naver.com>
2024-08-26 20:36:01 +0900

    #144 [feat] : viewmodel logout, withdraw repository 연결, state emit

--- feature/src/main/java/com/teamkkumul/feature/utils/dialog/DeleteDialogViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/dialog/DeleteDialogViewModel.kt
@@ -2,11 +2,14 @@ package com.teamkkumul.feature.utils.dialog
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamkkumul.core.data.repository.LoginRepository
 import com.teamkkumul.core.data.repository.MeetUpRepository
 import com.teamkkumul.core.data.repository.MyGroupRepository
+import com.teamkkumul.core.data.repository.UserInfoRepository
 import com.teamkkumul.core.ui.view.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,9 +18,17 @@ import javax.inject.Inject
 class DeleteDialogViewModel @Inject constructor(
     private val meetUpRepository: MeetUpRepository,
     private val myGroupRepository: MyGroupRepository,
+    private val loginRepository: LoginRepository,
+    private val userInfoRepository: UserInfoRepository,
 ) : ViewModel() {
     private val _deleteMyGroupState = MutableSharedFlow<UiState<Unit>>()
     val deleteMyGroupState get() = _deleteMyGroupState.asSharedFlow()
+
+    private val _logoutState = MutableSharedFlow<UiState<Unit>>()
+    val logoutState: SharedFlow<UiState<Unit>> = _logoutState.asSharedFlow()
+
+    private val _withdrawState = MutableSharedFlow<UiState<Unit>>()
+    val withdrawState: SharedFlow<UiState<Unit>> = _withdrawState.asSharedFlow()
 
     fun deleteMyGroup(meetingId: Int) {
         viewModelScope.launch {
@@ -26,6 +37,30 @@ class DeleteDialogViewModel @Inject constructor(
             }
                 .onFailure { exception ->
                     _deleteMyGroupState.emit(UiState.Failure(exception.message.toString()))
+                }
+        }
+    }
+
+    fun postLogout() {
+        viewModelScope.launch {
+            loginRepository.postLogout()
+                .onSuccess {
+                    _logoutState.emit(UiState.Success(Unit))
+                    userInfoRepository.clearAll()
+                }.onFailure {
+                    _logoutState.emit(UiState.Failure(it.message.toString()))
+                }
+        }
+    }
+
+    fun deleteWithdrawal() {
+        viewModelScope.launch {
+            loginRepository.deleteWithdrawal()
+                .onSuccess {
+                    _withdrawState.emit(UiState.Success(Unit))
+                    userInfoRepository.clearAll()
+                }.onFailure {
+                    _withdrawState.emit(UiState.Failure(it.message.toString()))
                 }
         }
     }
