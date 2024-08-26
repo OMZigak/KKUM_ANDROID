c66912d974bb7533d79264ea8b799b7b2045f192
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-08-27 03:21:16 +0900

    [feat/#146] ViewHolder 생성

---
+++ feature/src/main/java/com/teamkkumul/feature/meetupcreate/friend/viewholder/MeetUpCreateFriendViewHolder.kt
@@ -0,0 +1,35 @@
+package com.teamkkumul.feature.meetupcreate.friend.viewholder
+
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.ItemMeetUpCreateFriendBinding
+import com.teamkkumul.feature.utils.setEmptyImageUrl
+import com.teamkkumul.model.MyGroupMemberModel
+
+class MeetUpCreateFriendViewHolder(
+    private val binding: ItemMeetUpCreateFriendBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private val selectedDrawable: Drawable? =
+        ContextCompat.getDrawable(binding.root.context, R.drawable.sel_rv_main_selected_item)
+
+    fun onBind(data: MyGroupMemberModel.Member, isSelected: Boolean) {
+        with(binding) {
+            ivMeetUpCreateFriendProfileImage.setEmptyImageUrl(data.profileImg)
+            tvMyGroupCreateFrinedProfileName.text = data.name
+            updateBackground(isSelected)
+        }
+    }
+
+    private fun updateBackground(isSelected: Boolean) {
+        if (isSelected) {
+            binding.root.isActivated = true
+            selectedDrawable?.let { binding.root.background = it }
+        } else {
+            binding.root.isActivated = false
+            binding.root.background = null
+        }
+    }
+}
