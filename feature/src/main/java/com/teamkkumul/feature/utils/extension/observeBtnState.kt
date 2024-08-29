4f88259b47722a029f6a7e3da02557157090e4ce
chanu <cw8881@naver.com>
2024-08-29 17:34:50 +0900

    #126 [feat] : observeBtn state 확장함수로 변환

---
+++ feature/src/main/java/com/teamkkumul/feature/utils/extension/observeBtnState.kt
@@ -0,0 +1,45 @@
+package com.teamkkumul.feature.utils.extension
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.flowWithLifecycle
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import com.teamkkumul.core.ui.util.context.colorOf
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
+import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
+import com.teamkkumul.core.ui.view.setVisible
+import com.teamkkumul.feature.utils.model.BtnState
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+fun Fragment.observeBtnState(
+    stateFlow: StateFlow<BtnState>,
+    onStateChanged: (BtnState) -> Unit,
+) {
+    stateFlow.flowWithLifecycle(this.viewLifeCycle).onEach { state ->
+        onStateChanged(state)
+    }.launchIn(this.viewLifeCycleScope)
+}
+
+fun setUpButton(
+    state: BtnState,
+    button: MaterialButton,
+    circle: ImageView,
+    progressBar: LinearProgressIndicator,
+    progressBarEnd: LinearProgressIndicator?,
+    helpText: TextView,
+) {
+    button.apply {
+        setStrokeColorResource(state.strokeColor)
+        setTextColor(context.colorOf(state.textColor))
+        setBackgroundColor(context.colorOf(state.backGroundColor))
+        isEnabled = state.isEnabled
+    }
+    circle.setImageResource(state.circleImage)
+    progressBar.progress = state.progress
+    progressBarEnd?.progress = state.progress
+    helpText.setVisible(state.isHelpTextVisible)
+}
