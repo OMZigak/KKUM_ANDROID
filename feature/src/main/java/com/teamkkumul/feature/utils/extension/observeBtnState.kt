7ccf7c4c484f5d058690cd1d3c8de54628ae55a8
chanu <cw8881@naver.com>
2024-08-31 18:10:23 +0900

    #126 [fix] : helpText set invisible처리

--- feature/src/main/java/com/teamkkumul/feature/utils/extension/observeBtnState.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/extension/observeBtnState.kt
@@ -2,27 +2,11 @@ package com.teamkkumul.feature.utils.extension
 
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.flowWithLifecycle
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.teamkkumul.core.ui.util.context.colorOf
-import com.teamkkumul.core.ui.util.fragment.viewLifeCycle
-import com.teamkkumul.core.ui.util.fragment.viewLifeCycleScope
-import com.teamkkumul.core.ui.view.setVisible
+import com.teamkkumul.core.ui.view.setInvisible
 import com.teamkkumul.feature.utils.model.BtnState
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-
-fun Fragment.observeBtnState(
-    stateFlow: StateFlow<BtnState>,
-    onStateChanged: (BtnState) -> Unit,
-) {
-    stateFlow.flowWithLifecycle(this.viewLifeCycle).onEach { state ->
-        onStateChanged(state)
-    }.launchIn(this.viewLifeCycleScope)
-}
 
 fun setUpButton(
     state: BtnState,
@@ -41,5 +25,5 @@ fun setUpButton(
     circle.setImageResource(state.circleImage)
     progressBar.progress = state.progress
     progressBarEnd?.progress = state.progress
-    helpText.setVisible(state.isHelpTextVisible)
+    helpText.setInvisible(state.isHelpTextVisible)
 }
