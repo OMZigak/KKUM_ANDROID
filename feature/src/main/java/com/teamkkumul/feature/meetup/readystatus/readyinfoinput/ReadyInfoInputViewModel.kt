d0095f67f4c44170d6ca2e52234a981e9f57c2be
chanu <cw8881@naver.com>
2024-08-31 19:27:13 +0900

    #126 [feat] : editText text shardflow로 옵저빙 변경, vailidate combine활용

--- feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/readyinfoinput/ReadyInfoInputViewModel.kt
+++ feature/src/main/java/com/teamkkumul/feature/meetup/readystatus/readyinfoinput/ReadyInfoInputViewModel.kt
@@ -10,9 +10,13 @@ import com.teamkkumul.feature.utils.TimeStorage.END_MINUTE
 import com.teamkkumul.feature.utils.TimeStorage.START_TIME
 import com.teamkkumul.model.MeetUpDetailModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,21 +25,20 @@ class ReadyInfoInputViewModel @Inject constructor(
     private val homeRepository: HomeRepository,
     private val meetUpRepository: MeetUpRepository,
 ) : ViewModel() {
-
     private val _readyInputState = MutableStateFlow(false)
     val readyInputState: StateFlow<Boolean> get() = _readyInputState
 
-    private val _readyHour = MutableStateFlow(true)
-    val readyHour: StateFlow<Boolean> get() = _readyHour
+    private val _readyHour = MutableSharedFlow<Boolean>()
+    val readyHour: SharedFlow<Boolean> get() = _readyHour
 
-    private val _readyMinute = MutableStateFlow(true)
-    val readyMinute: StateFlow<Boolean> get() = _readyMinute
+    private val _readyMinute = MutableSharedFlow<Boolean>()
+    val readyMinute: SharedFlow<Boolean> get() = _readyMinute
 
-    private val _movingHour = MutableStateFlow(true)
-    val movingHour: StateFlow<Boolean> get() = _movingHour
+    private val _movingHour = MutableSharedFlow<Boolean>()
+    val movingHour: SharedFlow<Boolean> get() = _movingHour
 
-    private val _movingMinute = MutableStateFlow(true)
-    val movingMinute: StateFlow<Boolean> get() = _movingMinute
+    private val _movingMinute = MutableSharedFlow<Boolean>()
+    val movingMinute: SharedFlow<Boolean> get() = _movingMinute
 
     private val _patchReadyInfoInputState = MutableStateFlow<UiState<Boolean>>(UiState.Loading)
     val patchReadyInfoInputState: StateFlow<UiState<Boolean>> get() = _patchReadyInfoInputState
@@ -103,13 +106,16 @@ class ReadyInfoInputViewModel @Inject constructor(
 
     private fun validateForm() {
         viewModelScope.launch {
-            val isFormValid =
-                _readyHour.value &&
-                    _readyMinute.value &&
-                    _movingHour.value &&
-                    _movingMinute.value
-
-            _readyInputState.emit(isFormValid)
+            combine(
+                _readyHour,
+                _readyMinute,
+                _movingHour,
+                _movingMinute,
+            ) { readyHour, readyMinute, movingHour, movingMinute ->
+                readyHour && readyMinute && movingHour && movingMinute
+            }.collectLatest { isFormValid ->
+                _readyInputState.emit(isFormValid)
+            }
         }
     }
 }
