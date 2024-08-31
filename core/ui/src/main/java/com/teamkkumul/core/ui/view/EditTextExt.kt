f624400ee67f854c64c9a0cfe4f80eea7cbae1e9
chanu <cw8881@naver.com>
2024-08-31 21:33:11 +0900

    #126 [feat] : edit text extension curosr back 함수 정의

--- core/ui/src/main/java/com/teamkkumul/core/ui/view/EditTextExt.kt
+++ core/ui/src/main/java/com/teamkkumul/core/ui/view/EditTextExt.kt
@@ -3,10 +3,11 @@ package com.teamkkumul.core.ui.view
 import android.widget.EditText
 import androidx.core.widget.doAfterTextChanged
 
-fun EditText.moveCursorToEnd() {
+fun EditText.doAfterTextChangedWithCursor(onTextChanged: (String) -> Unit) {
     this.doAfterTextChanged { text ->
         if (!text.isNullOrEmpty()) {
             this.setSelection(text.length)
+            onTextChanged(text.toString())
         }
     }
 }
