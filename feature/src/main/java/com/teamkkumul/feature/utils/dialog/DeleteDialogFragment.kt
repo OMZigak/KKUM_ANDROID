042aff00ec70e7baae2aa23849c3dd631464bf14
yskim6772 <yskim6772@gmail.com>
2024-09-13 21:10:09 +0900

    #184 [chore] : fix navigation stack error

--- feature/src/main/java/com/teamkkumul/feature/utils/dialog/DeleteDialogFragment.kt
+++ feature/src/main/java/com/teamkkumul/feature/utils/dialog/DeleteDialogFragment.kt
@@ -100,6 +100,7 @@ class DeleteDialogFragment :
             when (it) {
                 is UiState.Success -> {
                     findNavController().popBackStack(R.id.fragment_meet_up_container, true)
+                    findNavController().navigate(R.id.fragment_my_group)
                     dismiss()
                 }
 
@@ -114,6 +115,7 @@ class DeleteDialogFragment :
             when (it) {
                 is UiState.Success -> {
                     findNavController().popBackStack(R.id.fragment_meet_up_container, true)
+                    findNavController().navigate(R.id.fragment_my_group)
                     dismiss()
                 }
 
