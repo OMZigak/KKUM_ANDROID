84d262bb1f1fbdc87721285f7aa7a07e84bc3d28
chanu <cw8881@naver.com>
2024-09-17 19:32:59 +0900

    #188 [add] : ready status comoponent 분리

---
+++ feature/src/main/java/com/teamkkumul/feature/home/component/KkumulReadyStatusFragment.kt
@@ -0,0 +1,11 @@
+package com.teamkkumul.feature.home.component
+
+import com.teamkkumul.core.ui.base.BindingFragment
+import com.teamkkumul.feature.R
+import com.teamkkumul.feature.databinding.FragmentKkumulReadyStatusBinding
+
+class KkumulReadyStatusFragment :
+    BindingFragment<FragmentKkumulReadyStatusBinding>(R.layout.fragment_kkumul_ready_status) {
+    override fun initView() {
+    }
+}
