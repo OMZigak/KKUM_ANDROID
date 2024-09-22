a44322f78279cc837ad8abfd8b073a5cd87d64d3
yskim6772 <yskim6772@gmail.com>
2024-09-22 17:56:29 +0900

    #191 [fix] : fix addNewGroupModel to parcelable data class

--- core/model/src/main/java/com/teamkkumul/model/AddNewGroupModel.kt
+++ core/model/src/main/java/com/teamkkumul/model/AddNewGroupModel.kt
@@ -1,6 +1,10 @@
 package com.teamkkumul.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class AddNewGroupModel(
     val meetingId: Int,
     val invitationCode: String,
-)
+) : Parcelable
