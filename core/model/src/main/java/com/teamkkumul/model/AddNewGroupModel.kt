79e82555807b9fb2044d90f7cfedb89ad3027729
chanu <cw8881@naver.com>
2024-09-24 18:58:42 +0900

    #191 [refact] : add screen type for AddNewGroupModel

--- core/model/src/main/java/com/teamkkumul/model/AddNewGroupModel.kt
+++ core/model/src/main/java/com/teamkkumul/model/AddNewGroupModel.kt
@@ -1,11 +1,12 @@
 package com.teamkkumul.model
 
 import android.os.Parcelable
+import com.teamkkumul.model.type.ScreenType
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class AddNewGroupModel(
     val meetingId: Int,
     val invitationCode: String,
-    val sourceFragment: String ?= null
+    val screenType: ScreenType? = null,
 ) : Parcelable
