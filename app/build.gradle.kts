37a41a7d456737ecef836649013ea54088bfada9
chanu <cw8881@naver.com>
2024-10-10 04:27:53 +0900

    #208 [chore] : 1.0.1 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.teamkkumul.kkumul"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
